aae7b92a37d78abff7dbf67ce58192b83f54abbf
manik712 <ranineela71@gmail.com>
2024-02-27 14:45:11 +0600

    add for loop

--- forLoop/forLoop.js
+++ forLoop/forLoop.js
@@ -1,6 +1,11 @@
-let books = ["a", "w", "r", "e"];
+// let books = ["a", "w", "r", "e"];
 
-for (i = 0; i < books.length; i++) {
+// for (i = 0; i < books.length; i++) {
+//   console.log(i);
+// }
+// console.log("hello", i);
+
+for (i = 100; i>0; i--) {
   console.log(i);
+  
 }
-console.log("hello", i);
