594a3874f2afbabc0b861ae621db123ba0ec399a
manik712 <ranineela71@gmail.com>
2024-03-02 22:55:54 +0600

    while loop problem name

--- forLoop/whileLoopP.js
+++ forLoop/whileLoopP.js
@@ -1,3 +1,14 @@
+/*
+Problem: Factorial Calculation
+
+Write a program that calculates the factorial of a given
+positive integer. The factorial of a
+number is the product of all positive
+integers up to that number.
+
+
+*/
+
 function calculateFactorial(number) {
   // Ensure the number is a positive integer
   number = Math.max(1, Math.floor(number));
@@ -8,12 +19,13 @@ function calculateFactorial(number) {
   console.log(`Calculating the factorial of ${number}:`);
 
   while (currentNumber <= number) {
-      factorial *= currentNumber;
-      currentNumber++;
+    // factorial *= currentNumber;
+    factorial = factorial * currentNumber;
+    currentNumber++;
   }
 
   console.log(`Factorial of ${number} is: ${factorial}`);
 }
 
-// Example: Calculate factorial of 5
+// Example: Calculate factorial of 7
 calculateFactorial(7);
