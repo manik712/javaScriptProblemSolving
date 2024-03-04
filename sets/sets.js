0a52de9580c0f9b5db185237b1fc911c482216d9
manik712 <ranineela71@gmail.com>
2024-03-04 12:18:00 +0600

    Unique Elements

--- sets/sets.js
+++ sets/sets.js
@@ -1,17 +1,17 @@
 //new Set()
 
-const letters = new Set(["a", "b", "c","d"]);
-console.log(letters);
+const letters = new Set(["a", "b", "c", "d"]);
+// console.log(letters);
 
 //add()
 
 letters.add("g");
-console.log(letters);
+// console.log(letters);
 
 //delete()
 
 letters.delete("a");
-console.log(letters);
+// console.log(letters);
 
 //has()
 
@@ -19,7 +19,7 @@ console.log(letters);
 return true or false
 */
 const sss = letters.has("s");
-console.log(sss);
+// console.log(sss);
 
 //forEach()
 
@@ -27,10 +27,43 @@ let text = "";
 letters.forEach(function (value) {
   text = text + value;
 });
-console.log(text);
+// console.log(text);
+
+const rrr = letters.values();
+
+// console.log(rrr);
+
+/*
 
 
-  
-const rrr= letters.values()
+ problem:Unique Elements
 
-console.log(rrr);
+
+Write a function findUniqueElements that takes two arrays as
+arguments and returns a new array containing only the unique elements 
+from both arrays. The order of the elements in the result 
+should be the same as the order in which they appear in the input arrays.
+
+
+
+
+*/
+
+function findUniqueElements(array1, array2) {
+  const uniqueElements = array1.concat(array2);
+
+  // console.log(uniqueElements,56);
+
+  const uniqueSets = new Set(uniqueElements);
+
+  // console.log(uniqueSets);
+  const uniqueArray = Array.from(uniqueSets);
+  console.log(uniqueArray);
+  return uniqueArray;
+}
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [1, 2, 4, 7];
+
+const newArray = findUniqueElements(arr1, arr2);
+// console.log(newArray);
