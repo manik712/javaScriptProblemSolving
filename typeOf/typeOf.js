026a93816c9fcd5c22d24c38781cd2d73ca3486a
manik712 <ranineela71@gmail.com>
2024-03-05 23:11:21 +0600

    typeof

--- typeOf/typeOf.js
+++ typeOf/typeOf.js
@@ -15,4 +15,23 @@ console.log(typeof myCar1);
 let myCar2 = 30;
 console.log(typeof myCar2);
 let boolean1 = false;
-console.log(typeof  boolean1);
+console.log(typeof boolean1);
+
+let myCar3 = {
+  car: "volvo",
+  bike: "yamaha",
+};
+console.log(typeof myCar3,24);
+
+let arr = [1, 2, 3, 4, 5, 6];
+
+console.log(typeof arr, 28);
+
+let date = new Date();
+console.log(typeof date,31);
+
+let pen
+console.log(typeof pen,34);
+
+let nan = NaN
+console.log(typeof nan,37);
