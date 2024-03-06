48d282b37e7d27026f4393a9c9483665ebd832dc
manik712 <ranineela71@gmail.com>
2024-03-07 01:00:55 +0600

    add type conversion

---
+++ jsTypeConversion/jsTypeConversion.js
@@ -0,0 +1,18 @@
+const number = Number("5.6")
+console.log( number);
+console.log(typeof number);
+
+
+const number1 =parseFloat("5.7")
+console.log(typeof number1,7);
+
+
+const number2 = parseInt(666.86)
+
+console.log(typeof number2);
+
+let y = "30"
+
+let x = +y
+
+console.log(typeof y ,typeof x);
