5d809e1ee9c5918b630cd9a8e5acf6bbefff8469
manik712 <ranineela71@gmail.com>
2024-03-08 17:16:45 +0600

    bitwise AND operation

---
+++ bitwiseProblem/bitWiseP.js
@@ -0,0 +1,15 @@
+/*
+Write a function that
+takes in two numbers and 
+ returns the result of performing a
+bitwise AND operation on them.
+
+
+*/
+
+function bitwiseAnd(num1, num2) {
+  const a = num1 & num2;
+  return a;
+}
+
+console.log(bitwiseAnd(5, 3)); // Output: 1
