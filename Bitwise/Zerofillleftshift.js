07f16a5bed9373f2732c7e24d10cd736af08c02e
manik712 <ranineela71@gmail.com>
2024-03-11 23:21:52 +0600

    Zero fill left shift

---
+++ Bitwise/Zerofillleftshift.js
@@ -0,0 +1,7 @@
+let number = 15;
+let binary = number.toString(2);
+// console.log(binary);
+let shiftedResult = number << 2;
+
+console.log("Original number in binary: " + binary);
+console.log("Result of zero-fill left shift by 2 positions: " + shiftedResult);
