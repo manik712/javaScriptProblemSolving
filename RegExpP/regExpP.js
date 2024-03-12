ce18a78543c87e756d2e0745cd233149f5426a4b
manik712 <ranineela71@gmail.com>
2024-03-12 23:31:09 +0600

    Problem: Extract Domain from URL

--- RegExpP/regExpP.js
+++ RegExpP/regExpP.js
@@ -17,3 +17,32 @@ console.log(isValidEmail("john.doe@company.co.uk")); // true
 console.log(isValidEmail("invalid.email@.com")); // false
 console.log(isValidEmail("user@domain")); // false
 console.log(isValidEmail(".username@domain.com")); // false
+
+//problem
+
+/*
+Write a JavaScript function that
+takes a URL as input and returns
+the domain part of it. Consider
+the following rules:
+
+*/
+function extractDomain(url) {
+  const domainRegex = /^(?:https?:\/\/)?(?:www\.)?([^\/]+)/;
+  const matches = url.match(domainRegex);
+  // console.log(matches);
+
+  // return matches ? matches[1] : null;
+
+  // if (matches) {
+  //   return matches[1];
+  // } else {
+  //   return null;
+  // }
+  return matches && matches.length > 1 ? matches[1] : null;
+}
+
+// Examples
+console.log(extractDomain("http://www.example.com")); // "www.example.com"
+console.log(extractDomain("https://subdomain.example.co.uk/path")); // "subdomain.example.co.uk"
+console.log(extractDomain("invalidurl1")); // null
