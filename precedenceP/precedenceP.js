03e848e9cece7797cefd7c7fbbf2a6a3b975cdec
manik712 <ranineela71@gmail.com>
2024-03-13 20:16:04 +0600

    precedence problem

---
+++ precedenceP/precedenceP.js
@@ -0,0 +1,22 @@
+
+//problem
+
+var x = 5;
+var y = 10;
+var z = 15;
+
+var result = x + y * z;
+
+// console.log(result);
+//155
+
+
+
+//problem
+var a = 3;
+var b = 4;
+var c = 5;
+
+var result = a * b + c / a - b;
+//9.6666
+console.log(result);
