84e1a5320f979ffd0cbd1b522a18077cadf66aaa
manik712 <ranineela71@gmail.com>
2024-03-13 23:27:02 +0600

    range error,URIError,Reference Error,Syntax Error,Type Error

--- JavaScriptErrors/JavaScriptErrors.js
+++ JavaScriptErrors/JavaScriptErrors.js
@@ -27,64 +27,59 @@ try {
 
 //throw exception
 
-let x = "5";
-try {
-  if (x == "") throw "empty";
-  if (isNaN(x)) throw "not a number";
-  x = Number(x);
-  if (x < 5) throw "too low";
-  if (x > 10) throw "too high";
-} catch (err) {
-  console.log(err);
-  return;
-}
+// let x = "5";
+// try {
+//   if (x == "") throw "empty";
+//   if (isNaN(x)) throw "not a number";
+//   x = Number(x);
+//   if (x < 5) throw "too low";
+//   if (x > 10) throw "too high";
+// } catch (err) {
+//   console.log(err);
+//   return;
+// }
 
 // console.log("code is ok");
 
 //throw  with object
 
+// let x1 = "15";
+// try {
+//   if (x1 == "") throw "empty";
+//   if (isNaN(x1)) throw "not a number";
+//   x = Number(x1);
+//   if (x1 < 5) throw "too low";
+//   if (x1 > 10)
+//     throw {
+//       message: "too high",
+//       name: "highError",
+//     };
+// } catch (err) {
+//   console.log(err);
+//   return;
+// }
 
-
-let x1 = "15";
-try {
-  if (x1 == "") throw "empty";
-  if (isNaN(x1)) throw "not a number";
-  x = Number(x1);
-  if (x1 < 5) throw "too low";
-  if (x1 > 10)
-    throw {
-      message: "too high",
-      name: "highError",
-    };
-} catch (err) {
-  console.log(err);
-  return;
-}
-
-console.log("code is ok");
-
-
+// console.log("code is ok");
 
 //throw with object with finally
 
-let x2 = "5";
-try {
-  if (x2 == "") throw "empty";
-  if (isNaN(x2)) throw "not a number";
-  x = Number(x2);
-  if (x2 < 5) throw "too low";
-  if (x2 > 10)
-    throw {
-      message: "too high",
-      name: "highError",
-    };
-} catch (err) {
-  console.log(err);
-  return;
-} finally {
-  console.log("code is ok, 88");
-}
-
+// let x2 = "5";
+// try {
+//   if (x2 == "") throw "empty";
+//   if (isNaN(x2)) throw "not a number";
+//   x = Number(x2);
+//   if (x2 < 5) throw "too low";
+//   if (x2 > 10)
+//     throw {
+//       message: "too high",
+//       name: "highError",
+//     };
+// } catch (err) {
+//   console.log(err);
+//   return;
+// } finally {
+//   console.log("code is ok, 88");
+// }
 
 /*
 Error Object Properties
@@ -95,12 +90,54 @@ message	      Sets or returns an error message (a string)
 
 */
 
+//range error
 
 let num = 1;
 try {
-   num.toPrecision(500);
+  num.toPrecision(500);
   console.log(a);
   // A number cannot have 500 significant digits
 } catch (err) {
-  console.log(err.message, 99);
+  console.log(err.name, 99);
+  //RangeError
+  //toPrecision() argument must be between 1 and 100 99
+}
+
+//reference error
+
+let x5 = 5;
+try {
+  x5 = y + 1; // y cannot be used (referenced)
+} catch (err) {
+  console.log(err.name);
+  //ReferenceError
+}
+
+//Syntax Error
+
+try {
+  eval("alert('Hello)"); // Missing ' will produce an error
+} catch (err) {
+  console.log(err.name, 122);
+  //SyntaxError
+}
+//Type Error
+
+let num1 = 1;
+try {
+  num1.toUpperCase(); // You cannot convert a number to upper case
+} catch (err) {
+  console.log(err.name);
+  //TypeError
+}
+
+//URI (Uniform Resource Identifier) Error
+
+
+try {
+  decodeURI("%%%");   // You cannot URI decode percent signs
+}
+catch(err) {
+console.log(err.name);
+//URIError
 }
