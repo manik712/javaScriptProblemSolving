1db68cfc860b4ab9fc39f1bc17622b2ffb6fc4be
manik712 <ranineela71@gmail.com>
2024-03-18 00:13:14 +0600

    first syntaxError then ReferenceError

--- JavaScriptHoisting/JavaScriptHoisting.js
+++ JavaScriptHoisting/JavaScriptHoisting.js
@@ -2,29 +2,51 @@
 //In JavaScript, a variable can be declared after it has been used.
 
 //Example 1
-x = 5; // Assign 5 to x
+x1 = 5; // Assign 5 to x
 
-var x; // Declare x
+var x1; // Declare x
 
 // Example 2
-var x; // Declare x
-x = 5; // Assign 5 to x
 
+var x2; // Declare x
+x2 = 5; // Assign 5 to x
 
 // Example 3
 
-x= undefined; // Assign undefined to x
-x =10
-console.log(x);
-var x; // declare 10 to x
+x3 = undefined; // Assign undefined to x
+x3 = 10;
+console.log(x3);
+var x3; // declare 10 to x
 
 //another case let and const are declared but not assigned undefined
 //example
-x=  //not undefined; // Assign undefined to x
-x =11
-console.log(x);
-let x; // declare
+
+x4 = //not undefined; // Assign undefined to x
+  x4 = 11;
+console.log(x4);
+let x4; // declare
 
 //same as const
+
 //let ,var and const are hosted
 //but only var is assigned
+//let and const are stay temporary death zones
+
+//error
+
+function foo() {
+  x = 8;
+  console.log(x);
+  let x; // declare
+}
+foo();
+//ReferenceError: Cannot access 'x' before initialization
+
+
+//syntaxError 
+
+carName = "aaaaa"
+const carName ;
+
+
+//first syntaxError then ReferenceError
