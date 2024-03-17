52609bb87381d0de1b251ad6eab1cd40b5a02dfd
manik712 <ranineela71@gmail.com>
2024-03-17 20:52:14 +0600

    scopes

--- JavaScriptScope/JavaScriptScope.js
+++ JavaScriptScope/JavaScriptScope.js
@@ -8,12 +8,7 @@ Global scope
 
 */
 
-
-
-
 //Block Scope
-
-
 
 /*
 Before ES6 (2015), JavaScript variables had only Global Scope and Function Scope.
@@ -26,20 +21,15 @@ Variables declared inside a { } block cannot be accessed from outside the block:
 
 */
 
-
 {
-
-  let x = 5
+  let x = 5;
 }
 
 //x can not  be used outside
 
 //Variables declared with the var keyword can NOT have block scope
 
-
-
 //Local Scope
-
 
 // code here can NOT use carName
 
@@ -49,3 +39,26 @@ function myFunction() {
 }
 
 // code here can NOT use carName
+
+function a() {
+  let x = 5;
+}
+//let x can declare, but they are not same
+function b() {
+  let x = 5;
+}
+
+function myFunction() {
+  var carName = "Volvo"; // Function Scope
+}
+
+function myFunction() {
+  let carName = "Volvo"; // Function Scope
+}
+
+function myFunction() {
+  const carName = "Volvo"; // Function Scope
+}
+
+//var global scope and function scope
+//let and const block scope
