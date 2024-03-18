178aaa768c416b9f5ae353d97e0f69eac47cfb3a
manik712 <ranineela71@gmail.com>
2024-03-19 00:24:03 +0600

    JavaScript Initializations are Not Hoisted

--- JavaScriptHoisting/JavaScriptHoisting.js
+++ JavaScriptHoisting/JavaScriptHoisting.js
@@ -21,10 +21,10 @@ var x3; // declare 10 to x
 //another case let and const are declared but not assigned undefined
 //example
 
-x4 = //not undefined; // Assign undefined to x
-  x4 = 11;
-console.log(x4);
-let x4; // declare
+// x4 = //not undefined; // Assign undefined to x
+//   x4 = 11;
+// console.log(x4);
+// let x4; // declare
 
 //same as const
 
@@ -34,19 +34,44 @@ let x4; // declare
 
 //error
 
-function foo() {
-  x = 8;
-  console.log(x);
-  let x; // declare
-}
-foo();
+// function foo() {
+//   x = 8;
+//   console.log(x);
+//   let x; // declare
+// }
+// foo();
 //ReferenceError: Cannot access 'x' before initialization
 
+//syntaxError
 
-//syntaxError 
-
-carName = "aaaaa"
-const carName ;
-
+carName = "aaaaa";
+// const carName ;
 
 //first syntaxError then ReferenceError
+
+/*
+Declare Your Variables At the Top !
+Hoisting is (to many developers) an unknown or overlooked behavior of JavaScript.
+
+If a developer doesn't understand hoisting, programs may contain bugs (errors).
+
+To avoid bugs, always declare all variables at the beginning of every scope.
+
+Since this is how JavaScript interprets the code, it is always a good rule.
+
+
+*/
+
+var a;
+var b;
+a = 10;
+console.log(a, b);
+// 10, undefined
+b = 11;
+/*
+
+JavaScript Initializations are Not Hoisted
+JavaScript only hoists declarations, not initializations.
+
+
+*/
