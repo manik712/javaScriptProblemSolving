3e2ffad23e32e47fb1110c97e666f67a995f9e7a
manik712 <ranineela71@gmail.com>
2024-03-18 15:26:41 +0600

    use filter

--- javaScriptProblem/problem1.js
+++ javaScriptProblem/problem1.js
@@ -2,20 +2,35 @@
 //Example : if arr = [1, 2, 3, 4, 5, 6, 2, 3] & num = 2
 //Result should be arr = [1, 3, 4, 5, 6, 3]
 
-function deleteOccurrences(arr, num) {
-  return arr.filter((element) => element != num);
-}
-arr = [1, 2, 3, 4, 5, 6, 2, 3];
-num = 2;
-const array = deleteOccurrences(arr, num);
-console.log(array);
+// function deleteOccurrences(arr, num) {
+//   return arr.filter((element) => element != num);
+// }
+// arr = [1, 2, 3, 4, 5, 6, 2, 3];
+// num = 2;
+// const array = deleteOccurrences(arr, num);
+// console.log(array);
 
 //or
 
-let numbers = [1, 2, 3, 4, 5, 6];
-let num = 2;
-let result = numbers.filter(lal);
-function lal(element) {
-  return element != num;
-}
-console.log(result);
+// let numbers = [1, 2, 3, 4, 5, 6];
+// let num = 2;
+// let result = numbers.filter(lal);
+// function lal(element) {
+//   return element != num;
+// }
+// console.log(result);
+
+// or
+
+let numbers1 = [1, 2, 3, 4, 5, 6,7];
+let num1 = 2;
+
+let answer = numbers1.filter((currentValue, index, arr) => {
+  console.log(currentValue,29);
+  console.log(index,30);
+  console.log(arr,31);
+  return currentValue != num1;
+});
+console.log(answer);
+
+
