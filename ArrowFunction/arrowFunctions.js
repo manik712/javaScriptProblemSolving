09a57190af7f10024b22e3b0df47b7561b0cf79f
manik712 <ranineela71@gmail.com>
2024-03-21 22:43:35 +0600

    arrow function

---
+++ ArrowFunction/arrowFunctions.js
@@ -0,0 +1,21 @@
+//Arrow functions were introduced in ES6.
+//Arrow functions allow us to write shorter function syntax
+let myFunction = (a, b) => a * b;
+
+//Before Arrow:
+
+hello = function () {
+  return "Hello World!";
+};
+
+//With Arrow Function:
+
+hello = () => {
+  return "Hello World!";
+}
+
+//Arrow Functions Return Value by Default:
+hello = () => "Hello World!";
+
+
+
