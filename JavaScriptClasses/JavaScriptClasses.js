666f0396395f4230b17c0b901f2ca505ed1e78de
manik712 <ranineela71@gmail.com>
2024-03-22 22:42:21 +0600

    The Constructor Method

--- JavaScriptClasses/JavaScriptClasses.js
+++ JavaScriptClasses/JavaScriptClasses.js
@@ -17,19 +17,68 @@ class ClassName {
 
 //example
 
-class car {
+class Car {
   constructor(name, age) {
     this.name = name;
     this.age = age;
   }
 }
+
+
+const car = new Car("manik", 34);
+console.log(car);
+
 //template
 //মাপনদণ্ড নিয়ামক
 
 //A JavaScript class is not an object.
 
-
-
-
 //Using a Class
 //When you have a class, you can use the class to create objects:
+
+//The Constructor Method
+/*
+
+
+
+The constructor method is a special method:
+
+It has to have the exact name "constructor"
+It is executed automatically when a new object is created
+It is used to initialize object properties
+
+
+
+*/
+
+//
+//If you do not define a constructor method, JavaScript will add an empty constructor method.
+
+/*
+lass methods are created with the same syntax as object methods.
+
+Use the keyword class to create a class.
+
+Always add a constructor() method.
+
+Then add any number of methods.
+
+
+*/
+
+// Syntax
+
+class ClassName {
+  constructor() {
+    c;
+  }
+  method_1() {
+    c;
+  }
+  method_2() {
+    c;
+  }
+  method_3() {
+    c;
+  }
+}
