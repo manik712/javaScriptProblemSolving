e3a4dbce34bdee84b11a33a803811f93ca8ebce9
manik712 <ranineela71@gmail.com>
2024-03-22 10:59:08 +0600

    javaScript class

---
+++ JavaScriptClasses/JavaScriptClasses.js
@@ -0,0 +1,35 @@
+//JavaScript Classes are templates for JavaScript Objects.
+
+//JavaScript Class Syntax
+/*
+Use the keyword class to create a class.
+
+Always add a method named constructor():
+
+
+*/
+
+class ClassName {
+  constructor() {
+    code;
+  }
+}
+
+//example
+
+class car {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+//template
+//মাপনদণ্ড নিয়ামক
+
+//A JavaScript class is not an object.
+
+
+
+
+//Using a Class
+//When you have a class, you can use the class to create objects:
