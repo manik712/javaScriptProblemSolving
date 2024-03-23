1f578eb977bf917291504d124edb0cf538d46c23
manik712 <ranineela71@gmail.com>
2024-03-23 20:54:44 +0600

    test.add and varA

--- JavaScriptModules/main.js
+++ JavaScriptModules/main.js
@@ -16,10 +16,24 @@ console.log(result);
 
 
 
+//varA 
+import { add as  varA } from "./apple.js"
+
+console.log(varA);
+
+
+
+
 
 // * as test 
 
 import * as test  from "./apple.js"
 
-console.log(test);
+console.log(test); // out put a object
 
+
+
+import * as test  from "./apple.js"
+
+console.log(test.add); // out put a object
+                       //11
