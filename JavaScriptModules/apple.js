50b641aa38304d76359d677f97a2db1003cad18a
manik712 <ranineela71@gmail.com>
2024-03-23 21:38:26 +0600

    default export declaration just one time not multiple times

--- JavaScriptModules/apple.js
+++ JavaScriptModules/apple.js
@@ -6,5 +6,11 @@ export function add(a, b, c) {
   return a + b + c;
 }
 
+export  const b = 56
 const a = 5;
 export default a;
+
+export  function myAim(){
+
+  console.log("my aim is to be good developer");
+}
--- JavaScriptModules/main.js
+++ JavaScriptModules/main.js
@@ -3,43 +3,40 @@
 // const result = add(2, 4, 5);
 // console.log(result);
 
-
-
-
 //
 
-import { add } from "./apple.js"
+import { add } from "./apple.js";
 
 const result = add(2, 4, 5);
 
 console.log(result);
 
-
-
-//varA 
-import { add as  varA } from "./apple.js"
+//varA
+import { add as varA } from "./apple.js";
 
 console.log(varA);
 
+// * as test
 
-
-
-
-// * as test 
-
-import * as test  from "./apple.js"
+import * as test from "./apple.js";
 
 console.log(test); // out put a object
-
-
-
 
 // import * as test  from "./apple.js"
 
 // console.log(test.add); // out put a object
 //                        //11
-                       
 
 
-import external from "./apple.js"
+
+
+import external, { b } from "./apple.js";
 console.log(external);
+console.log(b);
+
+
+//default declaration just one time not multiple times
+
+import { myAim } from "./apple.js";
+
+myAim()
