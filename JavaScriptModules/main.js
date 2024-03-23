1c63a36a4346612f39334af78f8a44451ae55836
manik712 <ranineela71@gmail.com>
2024-03-23 20:46:25 +0600

    import * as test

--- JavaScriptModules/main.js
+++ JavaScriptModules/main.js
@@ -4,8 +4,22 @@
 // console.log(result);
 
 
+
+
+//
+
 import { add } from "./apple.js"
 
 const result = add(2, 4, 5);
 
 console.log(result);
+
+
+
+
+// * as test 
+
+import * as test  from "./apple.js"
+
+console.log(test);
+
