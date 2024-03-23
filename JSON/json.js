bd692f53f5e075f99460e66d759cefd1e8001969
manik712 <ranineela71@gmail.com>
2024-03-23 21:54:25 +0600

    JavaScript JSON

---
+++ JSON/json.js
@@ -0,0 +1,62 @@
+//JavaScript JSON
+
+/*
+JSON is a format for storing and transporting data.
+
+JSON is often used when data is sent from a server to a web page.
+
+
+
+
+What is JSON?
+
+JSON stands for JavaScript Object Notation
+JSON is a lightweight data interchange format
+JSON is language independent *
+JSON is "self-describing" and easy to understand
+
+
+ The JSON syntax is derived from JavaScript 
+ object notation syntax, but the JSON format
+  is text only. Code for reading and generating 
+  JSON data can be written in any programming language.
+
+*/
+
+/*
+JSON Example
+This JSON syntax defines an employees object: an array of 3 employee records (objects):
+
+JSON Example
+
+
+
+*/
+
+{
+  "employees"[
+    ({ firstName: "John", lastName: "Doe" },
+    { firstName: "Anna", lastName: "Smith" },
+    { firstName: "Peter", lastName: "Jones" })
+  ];
+}
+
+/*
+The JSON format is syntactically identical to the code for creating JavaScript objects.
+
+Because of this similarity, a JavaScript program can easily convert JSON data into native JavaScript objects.
+
+
+
+JSON Syntax Rules
+Data is in name/value pairs
+Data is separated by commas
+Curly braces hold objects
+Square brackets hold arrays
+
+
+
+
+
+
+*/
