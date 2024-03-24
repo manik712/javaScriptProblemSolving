4492d1ab114629b986d3df9f638e84f8c48679ab
manik712 <ranineela71@gmail.com>
2024-03-24 10:22:09 +0600

    debugging

---
+++ Debugging/debugging.js
@@ -0,0 +1,39 @@
+//JavaScript Debugging
+
+//bug means error
+
+//Debug means to find errors
+
+/*
+there are two types of errors 
+1.syntax  error
+2.logical error
+
+Searching for (and fixing) errors in programming code is called code debugging.
+
+
+
+*/
+
+//
+//Errors can (will) happen, every time you write some new computer code.
+
+/*
+
+
+
+
+Code Debugging
+Programming code might contain syntax errors, or logical errors.
+
+Many of these errors are difficult to diagnose.
+
+Often, when programming code contains errors, nothing will happen. There are no error messages, and you will get no indications where to search for errors.
+
+Searching for (and fixing) errors in programming code is called code debugging.
+
+
+
+*/
+//JavaScript Debuggers
+
