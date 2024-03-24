904d13445d7b7a6e62e6aee1f84199986cf11baf
manik712 <ranineela71@gmail.com>
2024-03-24 19:08:28 +0600

    Line Length < 80

--- StyleGuide/StyleGuide.js
+++ StyleGuide/StyleGuide.js
@@ -61,5 +61,74 @@ const person = {
   firstName: "John",
   lastName: "Doe",
   age: 50,
+  eyeColor: "blue",
+};
+
+/*
+
+
+General rules for complex (compound) statements:
+
+Put the opening bracket at the end of the first line.
+Use one space before the opening bracket.
+Put the closing bracket on a new line, without leading spaces.
+Do not end a complex statement with a semicolon.
+Functions:
+
+
+
+
+*/
+
+//Functions:
+function toCelsius(fahrenheit) {
+  return (5 / 9) * (fahrenheit - 32);
+}
+
+//Loops:
+for (let i = 0; i < 5; i++) {
+  x += i;
+}
+
+//Conditionals:
+
+if (time < 20) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
+
+/*
+Object Rules
+General rules for object definitions:
+
+Place the opening bracket on the same line as the object name.
+Use colon plus one space between each property and its value.
+Use quotes around string values, not around numeric values.
+Do not add a comma after the last property-value pair.
+Place the closing bracket on a new line, without leading spaces.
+Always end an object definition with a semicolon
+
+
+
+
+
+
+
+
+
+*/
+
+// Example
+const person1 = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
   eyeColor: "blue"
 };
+
+//Short objects can be written compressed, on one line, using spaces only between properties, like this:
+const person2 = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+
+
+//Line Length < 80
