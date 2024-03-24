02ccad7ed16a2a6a4eed594a0cec3ffcf2cbdc22
manik712 <ranineela71@gmail.com>
2024-03-24 18:18:38 +0600

    JS program to find the sum of digits in a number by using reduce

--- javaScriptProblem/problem3.js
+++ javaScriptProblem/problem3.js
@@ -1,16 +1,38 @@
 //Write a JS program to find the sum of digits in a number.
 // Example : if number = 287152, sum = 25
 
+// let number = 287152;
+// // console.log(number);
+// let sNumber = number.toString();
+// // console.log(sNumber);
+// let sNumber1 = sNumber.split("").map(Number);
+// // console.log(sNumber1);
+
+// let sum = 0;
+// for (let i = 0; i < sNumber1.length; i++) {
+//   sum += sNumber1[i];
+// }
+// console.log(sum);
+// //25
+
+
+
+
+
+
+//by using reduce function
+
+
 let number = 287152;
 // console.log(number);
 let sNumber = number.toString();
 // console.log(sNumber);
-let sNumber1 = sNumber.split("").map(Number);
-// console.log(sNumber1);
+let sNumber1 = sNumber.split("").map(Number).reduce(getSum, 0);
 
-let sum = 0;
-for (let i = 0; i < sNumber1.length; i++) {
-  sum += sNumber1[i];
+// 
+
+
+function getSum(total,num){
+  return total+num
 }
-console.log(sum);
-//25
+console.log(sNumber1);
---
+++ javaScriptProblem/problem4.js
@@ -0,0 +1,11 @@
+// //Print the factorial of a number n.
+// [Factorial of a number n is the product of all positive integers less than or equal to a
+//   given positive integer and denoted by that integer. ]
+
+//   Example :
+//   7! (factorial of 7) = 1x2x3x4x5x6x7 = 5040
+//   5! (factorial of 5) = 1x2x3x4x5 = 120
+//   3! (factorial of 3) = 1x2x3 = 6
+//   0! Is always 1
+//   Qs5. Find the largest number in an array with only positive numbers.
+// //
