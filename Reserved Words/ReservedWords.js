3d786a3891c2133378b001aa19ac7d4c7248b3ef
manik712 <ranineela71@gmail.com>
2024-03-25 22:39:14 +0600

    Reserved Words

---
+++ Reserved Words/ReservedWords.js
@@ -0,0 +1,15 @@
+// Reserved Words
+
+/*
+HTML Event Handlers
+In addition you should avoid using the name of all HTML event handlers.
+
+Examples:
+onblur	onclick	onerror	onfocus
+onkeydown	onkeypress	onkeyup	onmouseover
+onload	onmouseup	onmousedown	onsubmit
+
+
+
+
+*/
