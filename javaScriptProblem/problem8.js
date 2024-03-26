5f88ab20011a45d9848f1dd22fc2b6394fd882f4
manik712 <ranineela71@gmail.com>
2024-03-26 21:36:26 +0600

    find out the number those are divided by 3,5 and both 3 and 5 upto 100.

--- javaScriptProblem/problem8.js
+++ javaScriptProblem/problem8.js
@@ -1,9 +1,32 @@
 //find out the number those are divided by 3,5 and both 3 and 5 upto 100.
 // divided by 3 or 5
 
-function getNumber() {
+// function getNumber() {
+//   let number = [];
+//   for (let i = 1; i <= 100; i++) {
+//     // console.log(i);
+
+//     if (i % 3 == 0 || i % 5 == 0) {
+//       // if (i % 3 == 0 && i % 5 == 0) {
+//       //[ 15, 30, 45, 60, 75, 90 ]
+//       number.push(i);
+//     }
+//   }
+//   return number;
+// }
+
+// console.log(getNumber());
+
+
+
+
+
+//or
+
+
+function getNumber(num) {
   let number = [];
-  for (let i = 1; i <= 100; i++) {
+  for (let i = 1; i <= num; i++) {
     // console.log(i);
 
     if (i % 3 == 0 || i % 5 == 0) {
@@ -15,4 +38,4 @@ function getNumber() {
   return number;
 }
 
-console.log(getNumber());
+console.log(getNumber(100));
