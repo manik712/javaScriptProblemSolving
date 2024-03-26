d33fafc3039df69b544181e66a499f7a67d18427
manik712 <ranineela71@gmail.com>
2024-03-26 21:26:16 +0600

    divided by 3 or 5

---
+++ javaScriptProblem/problem8.js
@@ -0,0 +1,16 @@
+//find out the number those are divided by 3,5 and both 3 and 5 upto 100.
+// divided by 3 or 5
+
+function getNumber() {
+  let number = [];
+  for (let i = 1; i <= 100; i++) {
+    // console.log(i);
+
+    if (i % 3 == 0 || i % 5 == 0) {
+      number.push(i);
+    }
+  }
+  return number;
+}
+
+console.log(getNumber());
