e4485425386a380f54f474b962f08b179a9b6728
manik712 <ranineela71@gmail.com>
2024-03-27 00:13:04 +0600

    use fofor loop for truthy value

--- javaScriptProblem/problem9.js
+++ javaScriptProblem/problem9.js
@@ -12,24 +12,30 @@ const mixedArray = [
   0,
 ];
 
-// function falseFree() {
-
-//   for (let i = 0; i <mixedArray.length ; i++) {
-//     if(mixedArray[i]){
-//       return true;
-//     }
-//   }
-// }
-
-// console.log(falseFree());
-
-
-
-const newArray = mixedArray.filter(function (currentValue) {
-  if (currentValue) {
-    return true;
-  } else {
-    return false;
+function falseFree() {
+  let arrays = [];
+  for (let i = 0; i < mixedArray.length; i++) {
+    if (mixedArray[i]) {
+      arrays.push(mixedArray[i]);
+    }
   }
-});
-console.log(newArray);
+  return arrays;
+}
+
+console.log(falseFree());
+
+
+
+
+
+
+
+
+// const newArray = mixedArray.filter(function (currentValue) {
+//   if (currentValue) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// });
+// console.log(newArray);
