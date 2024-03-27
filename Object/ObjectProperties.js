a8096d4225e284b3e43c5fa271e0964a6789b502
manik712 <ranineela71@gmail.com>
2024-03-27 22:26:28 +0600

    prototype

--- Object/ObjectProperties.js
+++ Object/ObjectProperties.js
@@ -18,11 +18,11 @@ Syntax
 
 //Adding New Properties
 
-const person = {
-  fname: " John",
+// const person = {
+//   fname: " John",
   lname: " Doe",
-  age: 25,
-};
+  // age: 25,
+// };
 person.country = "United States";
 // console.log(person);
 
@@ -30,6 +30,59 @@ person.age = 34;
 
 console.log(person);
 
-delete person.fname;
+// delete person.fname;
+// or
+delete person["fname"];
 
 console.log(person);
+
+//The delete operator should not be used on predefined JavaScript object properties. It can crash your application.
+
+/*
+
+
+
+Nested Objects
+Values in an object can be another object
+
+
+
+
+*/
+
+// myObj = {
+//   name: "John",
+//   age: 30,
+//   cars: {
+//     car1: "Ford",
+//     car2: "BMW",
+//     car3: "Fiat",
+//   },
+// };
+
+
+//You can access nested objects using the dot notation or the bracket notation:
+
+// Nested Arrays and Objects
+
+
+
+const myObj = {
+  name: "John",
+  age: 30,
+  cars: [
+    { name: "Ford", models: ["Fiesta", "Focus", "Mustang"] },
+    { name: "BMW", models: ["320", "X3", "X5"] },
+    { name: "Fiat", models: ["500", "Panda"] },
+  ],
+};
+
+
+
+//Property Attributes
+let a = new String("John");
+console.dir(a);
+//Property 
+
+
+
