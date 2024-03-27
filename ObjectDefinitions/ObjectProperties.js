ee35f3eea47ca565e054e20e238be318e2f525a9
manik712 <ranineela71@gmail.com>
2024-03-27 21:04:39 +0600

    Object Properties

---
+++ ObjectDefinitions/ObjectProperties.js
@@ -0,0 +1,27 @@
+//Object Properties
+
+//A JavaScript object is a collection of unordered properties.
+
+/*
+
+JavaScript for...in Loop
+The JavaScript for...in statement loops through the properties of an object.
+
+Syntax
+
+
+
+// */
+// for (let variable in object) {
+//   // code to be executed
+// }
+
+//Adding New Properties
+
+const person = {
+  fname: " John",
+  lname: " Doe",
+  age: 25,
+};
+person.country ="United States"
+console.log(person);
