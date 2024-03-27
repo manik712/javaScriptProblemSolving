c94115701c4be54395dc35f3bf47796f2960813d
manik712 <ranineela71@gmail.com>
2024-03-27 12:26:49 +0600

    find out truthy values from an object.

--- javaScriptProblem/problem10.js
+++ javaScriptProblem/problem10.js
@@ -1,24 +1,62 @@
-
 //find out truthy values from an object.
 
-const mixedArray = {
+// const mixedArray = {
+//   a: "hello",
+//   b: undefined,
+//   c: "manik",
+//   d: false,
+//   e: NaN,
+//   f: "67",
+//   g: "",
+//   h: "true",
+//   i: "j",
+//   j: null,
+//   k: 0,
+// };
 
+// const propertyValues = Object.values(mixedArray);
+// // console.log(propertyValues);
+// const values = propertyValues.filter(Boolean)
 
-  a :"hello",
-  b:undefined,
-  c:"manik",
-  d:false,
+// console.log(values);
+
+// function truthyObject() {
+//   for (let i in mixedArray) {
+//       // console.log((mixedArray[i]))
+//     if(mixedArray[i]){
+//       delete mixedArray[i];
+
+//     }
+//   }
+//   return mixedArray;
+// }
+
+// console.log(truthyObject());
+
+const obj = {
+  a: "hello",
+  b: undefined,
+  c: "manik",
+  d: false,
   e: NaN,
-  f:"67",
+  f: "67",
   g: "",
   h: "true",
-  i:"j",
+  i: "j",
   j: null,
-  k:0,
-      };
+  k: 0,
+};
 
-const propertyValues = Object.values(mixedArray);
-// console.log(propertyValues);
-const values = propertyValues.filter(Boolean)
+const s = function truthyObject(obj) {
+  for (let i in obj) {
+    // console.log(obj,58)
+    console.log(i, obj[i]);
+    // console.log();
+    if (!obj[i]) {
+      delete obj[i];
+    }
+  }
+  return obj;
+};
 
-console.log(values);
+console.log(s(obj));
