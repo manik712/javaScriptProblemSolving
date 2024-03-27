0f35ab720f703b0a7924b3cc2384844dc13ca7e2
manik712 <ranineela71@gmail.com>
2024-03-27 21:10:43 +0600

    updating

---
+++ Object/ObjectDefinitions.js
@@ -0,0 +1,93 @@
+//In JavaScript, objects are king. If you understand objects, you understand JavaScript.
+
+/*
+In JavaScript, almost "everything" is an object.
+
+Booleans can be objects (if defined with the new keyword)
+Numbers can be objects (if defined with the new keyword)
+Strings can be objects (if defined with the new keyword)
+Dates are always objects
+Maths are always objects
+Regular expressions are always objects
+Arrays are always objects
+Functions are always objects
+Objects are always ob
+
+*/
+
+//All JavaScript values, except primitives, are objects.
+
+//JavaScript Primitives
+
+//A primitive value is a value that has no properties or methods.
+
+// 3.14 is a primitive value
+
+//JavaScript defines 7 types of primitive data types:
+
+
+
+// Examples
+
+
+string
+number
+boolean
+null
+undefined
+symbol
+bigint
+
+
+/*
+
+
+Value	       Type	            Comment
+"Hello" 	  string	          "Hello" is always "Hello"
+3.14	      number	          3.14 is always 3.14
+true	       boolean	        true is always true
+false	      boolean	          false is always false
+null	      null (object)	    null is always null
+undefined	  undefined	        undefined is always undefined
+
+
+
+
+
+*/
+//A JavaScript object is a collection of named values
+
+// In JavaScript, there are two main categories of data types: primitive and non-primitive (also known as reference types).
+
+
+
+/*
+
+
+Primitive Data Types: These are immutable data types that directly store their values in memory. There are six primitive data types in JavaScript:
+
+Number: Represents numeric values, including integers and floating-point numbers.
+String: Represents sequences of characters, enclosed within single quotes (''), double quotes ("") or backticks (``).
+Boolean: Represents a logical value of true or false.
+Null: Represents the intentional absence of any value.
+Undefined: Represents the absence of a defined value.
+Symbol (added in ECMAScript 6/ES2015): Represents unique identifiers, primarily used as object property keys.
+
+
+
+
+
+Non-Primitive (Reference) Data Types: These are mutable data types that store references to their values. They are stored by reference in memory, meaning that when you create a variable of a non-primitive type, its value is stored as a reference to a location in memory where the actual value is stored. Non-primitive data types include:
+
+Object: Represents a collection of key-value pairs. Objects can be created using object literals, constructors, or classes. Examples include arrays, functions, and objects created with the new keyword.
+Array: A special type of object used for storing ordered collections of data.
+Function: A type of object that can be invoked.
+Date: Represents a specific moment in time.
+RegExp (Regular Expression): Represents a pattern used in string searching and manipulation.
+Primitive data types are passed by value, meaning that when you assign a primitive value to a variable or pass it as an argument to a function, a copy of the value is created. Non-primitive data types are passed by reference, meaning that when you assign a non-primitive value to a variable or pass it as an argument to a function, a reference to the original value in memory is passed.
+
+
+*/
+
+
+
