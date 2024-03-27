1fefb48cc58f4b786f722285967ad5c5608fd5d6
manik712 <ranineela71@gmail.com>
2024-03-27 23:51:43 +0600

    [object Object]

---
+++ Object/DisplayObjects.js
@@ -0,0 +1,20 @@
+//JavaScript Display Objects
+
+/*
+Some common solutions to display JavaScript objects are:
+
+Displaying the Object Properties by name
+Displaying the Object Properties in a Loop
+Displaying the Object using Object.values()
+
+
+*/
+const person = {
+  name: "John",
+  age: 30,
+  city: "New York"
+};
+
+
+
+document.getElementById("demo").innerHTML = person  
