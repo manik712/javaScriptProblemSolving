98cf202a4781e627df5d74b779b89a76803f3ba8
manik712 <ranineela71@gmail.com>
2024-03-28 11:32:44 +0600

    JavaScript Setter

--- Object/ObjectAccessors.js
+++ Object/ObjectAccessors.js
@@ -10,10 +10,30 @@ const person = {
   lastName: "Doe",
   language: "en",
 
-
   get lang() {
-    return this.firstName + " " + this.lastName + " " + this.language
-  }
+    return this.firstName + " " + this.lastName + " " + this.language;
+  },
 };
 
 console.log(person.lang);
+
+// JavaScript Setter (The set Keyword)
+
+
+
+const person1 = {
+  firstName: "John",
+  lastName: "Doe",
+  language: "",
+  set lang(lang) {
+    this.language = lang;
+  }
+};
+
+
+
+
+// Set an object property using a setter:
+person1.lang = "en";
+
+console.log(person1.language);
