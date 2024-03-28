ebeca48c133ef71a96959212260607640a2719e5
manik712 <ranineela71@gmail.com>
2024-03-28 11:56:57 +0600

    Why Using Getters and Setters

--- Object/ObjectAccessors.js
+++ Object/ObjectAccessors.js
@@ -19,21 +19,38 @@ console.log(person.lang);
 
 // JavaScript Setter (The set Keyword)
 
-
-
 const person1 = {
   firstName: "John",
   lastName: "Doe",
   language: "",
   set lang(lang) {
     this.language = lang;
-  }
+  },
 };
-
-
-
 
 // Set an object property using a setter:
 person1.lang = "en";
 
 console.log(person1.language);
+
+
+
+
+//Why Using Getters and Setters?
+
+/*
+
+It gives simpler syntax
+It allows equal syntax for properties and methods
+It can secure better data quality
+It is useful for doing things behind-the-scenes
+
+
+*/
+
+
+
+
+
+//Object.defineProperty()
+
