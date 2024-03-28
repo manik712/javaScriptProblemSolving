0e39f51e59db5c8545f5a498e7cdb59be8467479
manik712 <ranineela71@gmail.com>
2024-03-29 00:10:10 +0600

    Create a new array using the map function whose each element is equal to the original element plus 5.

--- javaScriptProblem/problem11.js
+++ javaScriptProblem/problem11.js
@@ -7,34 +7,11 @@ original element plus 5.
 
 */
 
+const arr = [4, 5, 7, 12];
+const newArr = arr.map(life);
 
-// const arr = [4, 5, 7, 12];
-// const newArr = arr.map(life);
-
-
-// function life(num) {
-//   return num + 5;
-// }
-
-// console.log(newArr);
-
-
-
-
-
-// Create a new array whose elements are in uppercase of words present in the original array.
-
-
-
-const arr =["manik","pen","jam"]
-
-const newArr = arr.map(words)
-
-function words(word) {
-
-return word.toUpperCase()
-
+function life(num) {
+  return num + 5;
 }
 
-console.log(newArr)
-  
+console.log(newArr);
---
+++ javaScriptProblem/problem12.js
@@ -0,0 +1,17 @@
+
+// Create a new array whose elements are in uppercase of words present in the original array.
+
+
+
+const arr =["manik","pen","jam"]
+
+const newArr = arr.map(words)
+
+function words(word) {
+
+return word.toUpperCase()
+
+}
+
+console.log(newArr)
+  
