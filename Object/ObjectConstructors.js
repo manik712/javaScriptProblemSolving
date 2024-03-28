a2081eaadd2f09d3513e383efd0b3cec5e84fed0
manik712 <ranineela71@gmail.com>
2024-03-28 20:31:48 +0600

    Adding a Property to an Object

--- Object/ObjectConstructors.js
+++ Object/ObjectConstructors.js
@@ -13,6 +13,27 @@ function Person(first, last, age, eye) {
 }
 
 const myPerson = new Person("manik","sarker","34")
+const myPerson1 = new Person("maniks","sarkers","35")
 
 console.log(myPerson.fullName());
+console.log(myPerson1.fullName());
+
 // manik sarker 34
+
+
+
+//It is considered good practice to name constructor functions with an upper-case first letter.
+
+
+
+
+//In a constructor function this does not have a value. 
+// It is a substitute for the new object. 
+// The value of this will become the new object when a new object is created.
+//this is not a variable. It is a keyword. You cannot change the value of this.
+
+
+//Adding a Property to an Object
+myPerson.country = "United States"
+console.log(myPerson);
+
