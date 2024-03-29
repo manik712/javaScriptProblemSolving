412312fc4b2520be6fc56eba4c7b0be86d2196f4
manik712 <ranineela71@gmail.com>
2024-03-29 20:23:02 +0600

    console.log(iteratorArr.next());

--- Object/JavaScriptiterables.js
+++ Object/JavaScriptiterables.js
@@ -1,38 +1,27 @@
 //JavaScript Iterables
 
-
-
 //Iterable objects are objects that can be iterated over with for..of.
 
 // Technically, iterables must implement the Symbol.iterator method.
 
-
-
 //Iterating Over a String
 // You can use a for..of loop to iterate over the elements of a string
-
 
 // Iterating Over an Array
 // You can use a for..of loop to iterate over the elements of an Array:
 
-
 // const arr=["a","b","c","d"];
 
-
-
-
-const arr={
-  one:1,
-  two:2,
-  three:3
-}
-
+const arr1 = {
+  one: 1,
+  two: 2,
+  three: 3,
+};
 
 // for(let i=0;i<arr.length;i++){
 
 //   console.log(i,arr[i]);
 // }
-
 
 // for(let x of arr){
 
@@ -41,13 +30,28 @@ const arr={
 
 //TypeError: arr is not iterable
 
-console.dir(arr);
+// console.dir(arr1);
 
 //will return object
 
-
-
-
+const arr = ["a", "b","c"];
 
 // which object has   Symbol(Symbol.iterator)  there are  Iterables
 
+console.log(arr[Symbol.iterator]);
+
+// will return ,ƒ values() ,itarator,
+
+console.log(arr[Symbol.iterator]());
+// Object [Array Iterator] {}
+
+const iteratorArr = arr[Symbol.iterator]();
+
+console.log(iteratorArr.next());
+//{ value: 'a', done: false }
+console.log(iteratorArr.next());
+//{ value: 'b', done: false }
+console.log(iteratorArr.next());
+//{ value: 'c', done: false }
+console.log(iteratorArr.next());
+//{ value: undefined, done: true }
