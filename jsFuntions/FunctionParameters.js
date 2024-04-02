f81405f37fa749c08212b0b961bb5f9e1a41bbec
manik712 <ranineela71@gmail.com>
2024-04-02 21:28:06 +0600

    Function Parameters

---
+++ jsFuntions/FunctionParameters.js
@@ -0,0 +1,27 @@
+//Function Parameters
+//A JavaScript function does not perform any checking on parameter values (arguments).
+
+const e = function functionName(parameter1, parameter2, parameter3) {
+  // code to be executed
+};
+
+console.log(e(3, 4, 5));
+//3,4,5 are arguments
+
+//Parameter Rules
+
+/*
+
+
+JavaScript function definitions do not specify data types for parameters.
+
+JavaScript functions do not perform type checking on the passed arguments.
+
+JavaScript functions do not check the number of arguments received.
+
+*/
+
+//JavaScript functions do not check the number of arguments received.
+const j = (a, b) => a * b;
+
+console.log(j(3, 4, 5, 7));
