e1176894f36ecca5d20dc0c92ab003cb57ddf93d
manik712 <ranineela71@gmail.com>
2024-04-02 23:16:53 +0600

    Function Rest Parameter

--- jsFuntions/FunctionParameters.js
+++ jsFuntions/FunctionParameters.js
@@ -94,3 +94,20 @@ console.log(a(h));
 //40
 console.log(h);
 //{manik: 8,sarker:5}
+
+
+
+// Function Rest Parameter
+// The rest parameter (...) allows a function to treat an indefinite number of arguments as an array:
+
+function sum(...args) {
+  let sum = 0;
+  for (let arg of args) sum += arg;
+  return sum;
+}
+
+let x11 = sum(4, 9, 16, 25, 29, 100, 66, 77);
+
+console.log(sum(x11));
+
+//326
