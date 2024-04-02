91c223e0e9b60ff4af60c60d5789dad562dc5d28
manik712 <ranineela71@gmail.com>
2024-04-02 18:20:57 +0600

    funtion arguments

--- jsFuntions/FunctionDefinitions.js
+++ jsFuntions/FunctionDefinitions.js
@@ -111,3 +111,41 @@ const y = function (a, b) {
 //ES6
 //arrow function
 const m = (a, b) => a * b;
+
+//or
+
+// const m = (a, b) => {
+// console.log("function");
+// return a * b;
+
+// }
+
+/*
+Arrow functions do not have their own this. They are not well suited for defining object methods.
+
+Arrow functions are not hoisted. They must be defined before they are used.
+
+Using const is safer than using var, because a function expression is always constant value.
+
+You can only omit the return keyword and the curly brackets if the function is a single statement. Because of this, it might be a good habit to always keep them:
+
+
+
+*/
+
+
+
+
+const n = function (a, b) {
+  console.log(arguments.length);
+  //[Arguments] { '0': 5, '1': 6 }
+  //console.log(arguments.length);
+  // 2
+  // console.log(arguments[1]);
+  // 6
+
+
+  return a * b;
+}
+console.log(n(5,6));
+// 
