a1886ff4fca5983285ac935f3165b75886a73acf
manik712 <ranineela71@gmail.com>
2024-04-03 20:22:14 +0600

    Function Invocation

--- jsFuntions/FunctionInvocation.js
+++ jsFuntions/FunctionInvocation.js
@@ -5,8 +5,66 @@
 //In this tutorial, we will use invoke, because a JavaScript function can be invoked without being called.
 
 // Invoking a Function as a Function
-function a(){
-  console.log("window object");
-}
-console.log(window.a());
+
+// function a() {
+//   console.log("window object");
+// }
+// console.log(window.a());
+
 //will return window object
+
+//What is this?
+
+function b() {
+  console.log(this);
+}
+
+b();
+//return
+/*
+<ref *1> Object [global] {
+  global: [Circular *1],
+  queueMicrotask: [Function: queueMicrotask],
+  clearImmediate: [Function: clearImmediate],
+  setImmediate: [Function: setImmediate] {
+    [Symbol(nodejs.util.promisify.custom)]: [Getter]
+  },
+  structuredClone: [Function: structuredClone],
+  clearInterval: [Function: clearInterval],
+  clearTimeout: [Function: clearTimeout],
+  setInterval: [Function: setInterval],
+  setTimeout: [Function: setTimeout] {
+    [Symbol(nodejs.util.promisify.custom)]: [Getter]
+  },
+  atob: [Function: atob],
+  btoa: [Function: btoa],
+  performance: Performance {
+    nodeTiming: PerformanceNodeTiming {
+      name: 'node',
+      entryType: 'node',
+      startTime: 0,
+      duration: 37.98820000002161,
+      nodeStart: 3.0081000002101064,
+      v8Start: 6.690500000026077,
+      bootstrapComplete: 28.371500000357628,
+      environment: 15.177300000097603,
+      loopStart: -1,
+      loopExit: -1,
+      idleTime: 0
+    },
+    timeOrigin: 1712149714179.662
+  },
+  fetch: [AsyncFunction: fetch]
+}
+
+
+
+*/
+
+const x = {
+  c: function () {
+    console.log(this);
+  },
+};
+
+x.c()
