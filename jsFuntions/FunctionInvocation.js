c6960c3cff28063e5c59312275bf57ae90ba3440
manik712 <ranineela71@gmail.com>
2024-04-03 09:41:36 +0600

    Function Invocation

---
+++ jsFuntions/FunctionInvocation.js
@@ -0,0 +1,12 @@
+//Function Invocation
+//The code inside a JavaScript function will execute when "something" invokes it.
+
+//Invoking a JavaScript Function
+//In this tutorial, we will use invoke, because a JavaScript function can be invoked without being called.
+
+// Invoking a Function as a Function
+function a(){
+  console.log("window object");
+}
+console.log(window.a());
+//will return window object
