59c4532fe04e1df1b97cef01d3c1c66db931087b
manik712 <ranineela71@gmail.com>
2024-04-03 23:45:04 +0600

    add apply method

--- jsFuntions/FunctionApply.js
+++ jsFuntions/FunctionApply.js
@@ -32,14 +32,40 @@ The apply() method accepts arguments in an array:
 
 */
 const person6 = {
-  fullName: function(city, country) {
+  fullName: function (city, country) {
     return this.firstName + " " + this.lastName + "," + city + "," + country;
-  }
-}
+  },
+};
 
 const person11 = {
-  firstName:"John",
-  lastName: "Doe"
-}
+  firstName: "John",
+  lastName: "Doe",
+};
 
-console.log(person6.fullName.apply(person11, ["Oslo", "Norway"]),41);
+console.log(person6.fullName.apply(person11, ["Oslo", "Norway"]), 41);
+
+//Simulate a Max Method on Arrays
+
+console.log(Math.max(1, 56, 990));
+
+
+console.log(Math.max.apply(null, [1,2,3])); 
+
+// Will also return 3
+
+/*
+The first argument (null) does not matter. It is not used in this example.
+
+These examples will give the same result:
+
+
+*/
+console.log(Math.max.apply(Math, [1,2,3]));
+// Will also return 3
+
+//or
+console.log(Math.max.apply(" ", [1,2,3]));
+// Will also return 3
+
+console.log(Math.max.apply(0, [1,2,3]));
+// Will also return 3
