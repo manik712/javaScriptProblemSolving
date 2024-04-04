fec8e060a493b7a594604fb884c6c688766cf4d4
manik712 <ranineela71@gmail.com>
2024-04-04 13:44:19 +0600

    a function can also access variables defined outside the function

--- jsFuntions/JavaScriptClosures.js
+++ jsFuntions/JavaScriptClosures.js
@@ -10,6 +10,20 @@
 
 function myFunction() {
   let a = 4;
+  //here the a is local variable
   return a * a
 };
 console.log(myFunction());
+
+
+
+
+//But a function can also access variables defined outside the function, like this:
+
+// Example
+let b = 5;
+//here the b is global variable
+function jFunction() {
+  return b * b;
+}
+console.log(jFunction());
