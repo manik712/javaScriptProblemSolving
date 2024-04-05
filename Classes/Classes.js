9718673de7c28f620de978808fca5cf99a8e7004
manik712 <ranineela71@gmail.com>
2024-04-05 12:58:54 +0600

    Class Methods

--- Classes/Classes.js
+++ Classes/Classes.js
@@ -15,18 +15,74 @@ class className {
   constructor() {}
 }
 //example
+
 class Car {
-
-  constructor(name ,year){
-    this.name = name ;
+  constructor(name, year) {
+    this.name = name;
     this.year = year;
-
   }
 }
-const car1 =new Car("COROLLA",1990)
+const car1 = new Car("COROLLA", 1990);
 console.log(typeof car1);
+
 // object
 // console.log(typeof Car);
 //function
+// A JavaScript class is not an object.
+// It is a template for JavaScript objects.
 console.log(car1);
 //Car { name: 'COROLLA', year: 1990 }
+
+//The constructor method is called automatically when a new object is created.
+
+/*
+The Constructor Method
+The constructor method is a special method:
+
+It has to have the exact name "constructor"
+It is executed automatically when a new object is created
+It is used to initialize object properties
+If you do not define a constructor method, JavaScript will add an empty constructor method.
+
+*/
+
+//Class Methods
+/*
+Class methods are created with the same syntax as object methods.
+
+Use the keyword class to create a class.
+
+Always add a constructor() method.
+
+Then add any number of methods.
+
+
+*/
+
+
+
+/*
+
+class ClassName {
+  constructor() { ... }
+  method_1() { ... }
+  method_2() { ... }
+  method_3() { ... }
+}
+
+*/
+
+
+class Bus {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+
+  run(){
+    console.log(this.name + " "+"is running");
+  }
+}
+const car2 = new Bus("COROLLA", 1990);
+car2.run();
+console.log(car2);
