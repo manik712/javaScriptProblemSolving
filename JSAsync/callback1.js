ea87a738a8b9076d990c4b256c236510ec2c6220
manik712 <ranineela71@gmail.com>
2024-04-08 16:14:10 +0600

    add propose and kissAndMarry function

--- JSAsync/callback1.js
+++ JSAsync/callback1.js
@@ -1,17 +1,17 @@
 let age = 18;
 let beautiful = true;
+let money = true;
+
 function choiceAGirl(callback) {
   console.log("i am trying to choice a girl");
   setTimeout(function () {
     if (age >= 18) {
       callback();
     } else {
-      console.log("you are not appropriate to get a girl");
+      console.log("you are not appropriate to choice a girl");
     }
   }, 3000);
 }
-
-
 
 function proposeAGirl(callback) {
   console.log("i am trying to propose a girl");
@@ -25,3 +25,25 @@ function proposeAGirl(callback) {
   }, 3000);
 }
 
+function propose(callback) {
+  console.log("propose a girl");
+  setTimeout(function () {
+    if (money) {
+      callback();
+    } else {
+      console.log("you are gorib,don't try to do this again");
+    }
+  }, 3000);
+}
+
+function kissAndMarry() {
+  console.log("she kiss me and marry me");
+}
+
+choiceAGirl(function () {
+  proposeAGirl(function () {
+    propose(function () {
+      kissAndMarry();
+    });
+  });
+});
