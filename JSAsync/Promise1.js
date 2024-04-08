0cc6c284afc687a1ef290f1fc95346fff7f8d751
manik712 <ranineela71@gmail.com>
2024-04-08 21:22:40 +0600

    add proposeAGirl function

--- JSAsync/Promise1.js
+++ JSAsync/Promise1.js
@@ -29,35 +29,3 @@ function proposeAGirl() {
   });
   return promise;
 }
-
-function propose() {
-  console.log("I propose a girl");
-  const promise = new Promise(function (res, rej) {
-    setTimeout(function () {
-      if (money) {
-        res();
-      } else {
-        rej("you are gorib,don't try to do this again");
-      }
-    }, 3000);
-  });
-  return promise;
-}
-
-function kissAndMarry() {
-  console.log("She kisses  me and marry me");
-  const promise = new Promise(function () {
-    setTimeout(function () {}, 2000);
-  });
-  return promise;
-}
-
-// call promise
-choiceAGirl()
-  .then(proposeAGirl)
-  .then(propose)
-  .then(kissAndMarry)
-  .then(function () {})
-  .catch(function (err) {
-    console.log(err);
-  });
