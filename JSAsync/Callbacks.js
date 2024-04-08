38ee6f2e78b7c3a1812bbbbaeb7ce8cbf34ac32a
manik712 <ranineela71@gmail.com>
2024-04-08 12:03:19 +0600

    callback function

--- JSAsync/Callbacks.js
+++ JSAsync/Callbacks.js
@@ -1,21 +1,18 @@
 //JavaScript Callbacks
 
-// function Display(some){
-//   console.log(some);
-// }
+function Display(some) {
+  console.log(some);
+}
 
-// function calculator(num1, num2){
-// return num1 + num2;
-// }
+function calculator(num1, num2) {
+  return num1 + num2;
+}
 
-// let result= calculator(1,100);
+let result = calculator(1, 100);
 
-// Display(result);
-
-
+Display(result);
 
 //or
-
 
 function Display(some) {
   console.log(some);
@@ -28,3 +25,26 @@ function calculator(num1, num2) {
 calculator(1, 100);
 
 // Display(result);
+
+//JavaScript Callbacks
+//A callback is a function passed as an argument to another function.
+
+//callBack function
+
+
+
+
+
+
+
+function Display(something) {
+  console.log("result:", something);
+}
+
+function calculator(num1, num2, callback) {
+  let sum = num1 + num2;
+   if(callback) callback(sum);
+}
+calculator(1, 100, Display);
+
+
