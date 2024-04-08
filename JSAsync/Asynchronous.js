5a7da1567e7995cb1fed1cef811b4e1872ee01ff
manik712 <ranineela71@gmail.com>
2024-04-08 13:37:18 +0600

    When using the JavaScript function setTimeout(), can specify a callback function to be executed on time-out

--- JSAsync/Asynchronous.js
+++ JSAsync/Asynchronous.js
@@ -10,8 +10,6 @@ console.log("line 3 ");
 
 //or
 
-
-
 //synchronous
 function test() {
   console.log("line 2 ");
@@ -24,12 +22,11 @@ console.log("line 3 ");
 // line 2
 // line 3
 
-
 //Asynchronous
 console.log("line 1 ");
-setTimeout(function(){
+setTimeout(function () {
   console.log("line 2 ");
-},200)
+}, 200);
 console.log("line 3 ");
 // line 1
 // line 3
@@ -38,8 +35,21 @@ console.log("line 3 ");
 //setInterval()
 console.log("line 1 ");
 
-// setInterval(function(){
-//   console.log("line 2 ");
-// },200000)
+setInterval(function () {
+  console.log("line 2 ");
+}, 5000);
 
 console.log("line 3 ");
+
+//In the real world, callbacks are most often used with asynchronous functions.
+// A typical example is JavaScript setTimeout().
+
+
+// again, callbacks are
+
+setTimeout(later, 4000);
+
+function later() {
+  console.log("call after 4s");
+}
+
