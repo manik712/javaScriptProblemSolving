92095d31d682c3088c40d0de6f0b28f701f444bf
manik712 <ranineela71@gmail.com>
2024-04-09 22:25:11 +0600

    Dynamic HTML content and add date()

---
+++ HTMLDOM/domH.js
@@ -0,0 +1,6 @@
+// Changing HTML Content
+document.getElementById(id).innerHTML = new HTML
+//Changing the Value of an Attribute
+document.getElementById(id).attribute = new value
+//Dynamic HTML content
+
