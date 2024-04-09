31a0f7cbc0ad2f7997b674aef10d5470645e044d
manik712 <ranineela71@gmail.com>
2024-04-09 10:50:04 +0600

    JavaScript HTML DOM Document

---
+++ HTMLDOM/domD.js
@@ -0,0 +1,15 @@
+//Finding HTML Elements
+
+
+/* 
+
+
+Method	                                              Description
+document.getElementById(id)                 	       Find an element by element id
+document.getElementsByTagName(name)	                 Find elements by tag name
+document.getElementsByClassName(name)	               Find elements by class name
+
+*/
+
+
+
