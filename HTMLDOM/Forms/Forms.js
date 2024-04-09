cc31bf06010ab5d599eb50da33f8f8efb4d744ed
manik712 <ranineela71@gmail.com>
2024-04-09 23:26:20 +0600

    add validation

--- HTMLDOM/Forms/Forms.js
+++ HTMLDOM/Forms/Forms.js
@@ -1,6 +1,9 @@
-function validateForm(){
-
-  const form =document.forms['myForm']
-  console.log(form);
-  return false;
+function validateForm() {
+  const form = document.forms["myForm"];
+  // console.log(form["fname"].value);
+  const value = form["fname"].value;
+  if (value === "") {
+    alert("Please enter a valid name");
+    return false;
+  }
 }
