83612d5af966f5710b4e48f318c11f9d5fd71bf5
manik712 <ranineela71@gmail.com>
2024-04-09 20:35:31 +0600

    Create a new array whose elements are in uppercase of words present in the original array.

---
+++ javaScriptProblem/problem13.js
@@ -0,0 +1,13 @@
+/*
+
+
+Create a new array whose elements are in uppercase of words present in the
+original array.
+
+
+*/
+
+const array = ["hello", "world", "mango"];
+
+const uppercase=array.map(arr=>arr.toUpperCase())
+console.log(uppercase);
