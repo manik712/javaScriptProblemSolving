06f9531c03b09ac58741cc2fcb1b5f3847eb63f8
manik712 <ranineela71@gmail.com>
2024-04-09 07:22:52 +0600

    dom

---
+++ HTMLDOM/HtmlDom.js
@@ -0,0 +1,12 @@
+// HTML DOM
+/*
+
+The HTML DOM is a standard object model and programming interface for HTML. It defines:
+
+The HTML elements as objects
+The properties of all HTML elements
+The methods to access all HTML elements
+The events for all HTML elements
+
+*/
+
