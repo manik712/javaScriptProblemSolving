5230508ed9dbeedc70054dafe8e4121270115004
manik712 <ranineela71@gmail.com>
2024-04-10 15:23:36 +0600

    add date

--- HTMLDOM/DOMEvents/DomEvents.js
+++ HTMLDOM/DOMEvents/DomEvents.js
@@ -1,15 +1,17 @@
-function clickHer(){
+function clickHer() {
   const event = document.getElementById("h2");
-  event.innerHTML ="neela"
+  event.innerHTML = "neela";
 }
-
-
 
 //or
 
+function clickHim(parameter) {
+  parameter.innerHTML = "jace";
+}
 
-function clickHim(id){
-  console.log(id);
-  id.innerHTML = "jace";
+//HTML Event Attributes
 
+function displayDate() {
+  const a = document.getElementById("h4");
+  a.innerHTML = Date();
 }
