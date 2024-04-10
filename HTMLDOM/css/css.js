904207fc0440be148361493a4e618459d73ac1a6
manik712 <ranineela71@gmail.com>
2024-04-10 09:38:10 +0600

    Using Events

--- HTMLDOM/css/css.js
+++ HTMLDOM/css/css.js
@@ -1,14 +1,23 @@
-const p = document.getElementById("p1");
-// const p = document.querySelector('#p1');
-p.style.color = "blue";
-p.style.fontSize = "200px";
+// const p = document.getElementById("p1");
+// // const p = document.querySelector('#p1');
+// p.style.color = "blue";
+// p.style.fontSize = "200px";
 
 //at first load the html file ,then load the javascript file
 
-setTimeout(function () {
+// setTimeout(function () {
+//   const p = document.getElementById("p1");
+//   // const p = document.querySelector('#p1');
+//   p.style.color = "blue";
+//   p.style.fontSize = "200px";
+// }, 4000);
+
+//Using Events
+
+
+function styling() {
   const p = document.getElementById("p1");
   // const p = document.querySelector('#p1');
   p.style.color = "blue";
   p.style.fontSize = "200px";
-}, 4000);
-
+}
