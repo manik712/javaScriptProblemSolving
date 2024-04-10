c16e24c6b2e0aad8e291ac8cf6991d638c9a0c37
manik712 <ranineela71@gmail.com>
2024-04-10 09:30:19 +0600

    at first load the html file ,then load the javascript file

--- HTMLDOM/css/css.js
+++ HTMLDOM/css/css.js
@@ -1,3 +1,14 @@
-const p = document.getElementById("p1")
+const p = document.getElementById("p1");
 // const p = document.querySelector('#p1');
 p.style.color = "blue";
+p.style.fontSize = "200px";
+
+//at first load the html file ,then load the javascript file
+
+setTimeout(function () {
+  const p = document.getElementById("p1");
+  // const p = document.querySelector('#p1');
+  p.style.color = "blue";
+  p.style.fontSize = "200px";
+}, 4000);
+
