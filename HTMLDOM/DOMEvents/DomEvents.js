49347bd83beab980264c1d24ebfd895b8cd57c57
manik712 <ranineela71@gmail.com>
2024-04-10 15:14:37 +0600

    send parameter into function

--- HTMLDOM/DOMEvents/DomEvents.js
+++ HTMLDOM/DOMEvents/DomEvents.js
@@ -2,3 +2,14 @@ function clickHer(){
   const event = document.getElementById("h2");
   event.innerHTML ="neela"
 }
+
+
+
+//or
+
+
+function clickHim(id){
+  console.log(id);
+  id.innerHTML = "jace";
+
+}
