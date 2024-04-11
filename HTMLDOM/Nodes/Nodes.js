e09de7c076611fd3d9c9fca47f60505342e9e832
manik712 <ranineela71@gmail.com>
2024-04-11 11:06:00 +0600

    Creating new HTML Elements - insertBefore()

--- HTMLDOM/Nodes/Nodes.js
+++ HTMLDOM/Nodes/Nodes.js
@@ -3,7 +3,11 @@ const node = document.createTextNode("hello world");
 para.appendChild(node);
 console.log(para.appendChild(node));
 //hello world
-const element = document.getElementById('div1');
+const element = document.getElementById("div1");
 element.appendChild(para);
 console.log(element.appendChild(para));
 //<p>hello world</p>
+
+//Creating new HTML Elements - insertBefore()
+const p1 = document.getElementById("p1");
+element.insertBefore(para,p1)
