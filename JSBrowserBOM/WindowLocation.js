613e9b8e8383d226559ba8c071ebac3831fec135
manik712 <ranineela71@gmail.com>
2024-04-16 11:12:32 +0600

    Window Location

---
+++ JSBrowserBOM/WindowLocation.js
@@ -0,0 +1,8 @@
+//Window Location
+//The window.location object can be used to get the current page address (URL) and to redirect the browser to a new page.
+
+const href = document.getElementById("href");
+
+
+
+href.innerHTML = "window href"+ location.href
