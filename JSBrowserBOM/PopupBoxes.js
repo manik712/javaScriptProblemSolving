1ba9c48364e7cf4707a22f204f5e1cedeb222f0f
manik712 <ranineela71@gmail.com>
2024-04-17 22:58:56 +0600

    add ConfirmBox()

--- JSBrowserBOM/PopupBoxes.js
+++ JSBrowserBOM/PopupBoxes.js
@@ -1,11 +1,24 @@
 // Popup Boxes
-//JavaScript has three kind of popup boxes: 
+//JavaScript has three kind of popup boxes:
 // Alert box,
 // Confirm box,
-  // and
-  
+// and
+
 // Prompt box.
 
-function alertBox(){
-  alert("hello world")
+const confirm = document.getElementById("confirm");
+
+function alertBox() {
+  alert("hello world");
+}
+
+function ConfirmBox() {
+  let text;
+  if (window.confirm("please ...")) {
+    text = "you press the confirm button";
+  } else {
+    text = "you press the cancel button";
+  }
+
+  confirm.innerHTML = text;
 }
