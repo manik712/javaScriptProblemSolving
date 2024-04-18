f48b1b9891b5dc1cc9d1b9d2c8349798eed0c73b
manik712 <ranineela71@gmail.com>
2024-04-18 08:26:16 +0600

    add  stopInterval

--- JSBrowserBOM/TimingEvents.js
+++ JSBrowserBOM/TimingEvents.js
@@ -1,24 +1,35 @@
 // Timing Events
-const setTimeout = document.getElementById("setTimeout");
-const setInterval = document.getElementById("setInterval");
-
+const setTimeouts = document.getElementById("setTimeouts");
+const setIntervals = document.getElementById("setIntervals");
 
 //timeOut()
+let myTimeOut;
 function timeOut() {
-  console.log("Time Out");
+  // console.log("Time Out");
+  myTimeOut = setTimeout(function () {
+    setTimeouts.innerHTML = "time is over!";
+  }, 3000);
 }
 
-
 //stopTimeOut()
-function stopTimeOut() {}
+function stopTimeOut() {
+  // console.log("Stop Time Out");
+  clearTimeout(myTimeOut);
+}
 
-
+let myInterval;
 //interval()
-function interval() {
-  console.log("Interval");
+   function interval() {
+  // console.log("Interval");
+   myInterval= setInterval(function () {
+    setIntervals.innerHTML = new Date().toLocaleTimeString();
+  }, 1000);
 }
 
 
 //stopInterval()
 
-function stopInterval() {}
+function stopInterval() {
+  // console.log("Stop Interval");
+  clearInterval(myInterval)
+}
