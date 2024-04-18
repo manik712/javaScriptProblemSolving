cb6c5646042a8fd14300eb0ea44e3fafbdf15418
manik712 <ranineela71@gmail.com>
2024-04-18 21:03:45 +0600

    JavaScript Cookies

---
+++ JSBrowserBOM/JavaScriptCookies.js
@@ -0,0 +1,24 @@
+// JavaScript Cookies
+//Cookies let you store user information in web pages.
+//Cookies are data, stored in small text files, on your computer.
+
+//setCookie()
+
+function setCookie(){
+  console.log("setting cookie");
+}
+
+//getNameCookie()
+function getNameCookie(){
+  console.log("getting SetCookie");
+}
+
+//setUserNameCookie()
+function setUserNameCookie(){
+  console.log("setting UserNameCookie");
+}
+
+//getUserNameCookie()
+function getUserNameCookie(){
+  console.log("getting SetUserNameCookie");
+}
