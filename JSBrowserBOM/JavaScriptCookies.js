1a48176a08e826200d0f245f35ea8059265a391e
manik712 <ranineela71@gmail.com>
2024-04-19 00:19:15 +0600

    add

--- JSBrowserBOM/JavaScriptCookies.js
+++ JSBrowserBOM/JavaScriptCookies.js
@@ -4,21 +4,42 @@
 
 //setCookie()
 
-function setCookie(){
-  console.log("setting cookie");
+function setCookie(cname, cValue, eXdays) {
+  // console.log("setting cookie");
+  const d = new Date();
+  // console.log(d,11);
+  let e = d.setTime(d.getTime() + eXdays * 24 * 60 * 60 * 1000);
+  //  console.log(d,13);
+  // console.log(e,14);
+  let expires = "expires=" + d.toUTCString();
+  // console.log(expires);
+  document.cookie = cname + "=" + cValue + ";expires" + "path=/";
+  // console.log(document.cookie);
 }
 
 //getNameCookie()
-function getNameCookie(){
-  console.log("getting SetCookie");
+function getCookie(cname) {
+  console.log(document.cookie);
+  //name=Manik; userName=Jace
+
+  let ca =document.cookie.split(';');
+  console.log(ca);
+  //['name=Manik', ' userName=Jace']
+  for(i=1; i<ca.length;i++){
+  let c = ca[i]
+  console.log(c);
+  }
+  
+  
 }
 
 //setUserNameCookie()
-function setUserNameCookie(){
+function setUserNameCookie() {
+  
   console.log("setting UserNameCookie");
 }
 
 //getUserNameCookie()
-function getUserNameCookie(){
+function getUserNameCookie() {
   console.log("getting SetUserNameCookie");
 }
