5a8b31b897492a48da5470c19887028299e53381
manik712 <ranineela71@gmail.com>
2024-04-20 08:16:20 +0600

    forms api

--- JSWebAPIs/JSWebAPIs.js
+++ JSWebAPIs/JSWebAPIs.js
@@ -1,10 +1,20 @@
 // JS Web APIs
 
-function myFunction(){
+function myFunction() {
   // console.log("clicking myFunction");
-  const validationObject =document.getElementById("id1")
-  if(!validationObject.checkValidity()){
-     document.getElementById("demo").innerHTML =validationObject.validationMessage
+  const validationObject = document.getElementById("id1");
+  if (validationObject.validity.rangeOverflow) {
+    validationObject.setCustomValidity("you have made rangeOverflow");
+  }else if (validationObject.validity.rangeUnderflow){
+    validationObject.setCustomValidity("you have made rangeUnderflow")
+  }else if(validationObject.validity.typeMismatch){
+    validationObject.setCustomValidity("you have made type error")
+  }else if(validationObject.validity.tooLong){
+    validationObject.setCustomValidity("you have made too long")
   }
-  
+
+  if (!validationObject.checkValidity()) {
+    document.getElementById("demo").innerHTML =
+      validationObject.validationMessage;
+  }
 }
