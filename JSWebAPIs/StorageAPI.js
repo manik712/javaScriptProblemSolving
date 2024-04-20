9431ec928540de3299210d12bb85a4d4c46b1fa3
manik712 <ranineela71@gmail.com>
2024-04-20 13:09:43 +0600

    Storage API

---
+++ JSWebAPIs/StorageAPI.js
@@ -0,0 +1,23 @@
+// Storage API
+function myLocalStorage(key,value){
+  localStorage.setItem(key,value);
+
+}
+// 
+function  getLocalStorage(key){
+ alert( localStorage.getItem(key))
+
+}
+
+function  removeLocalStorage(key){
+  localStorage.removeItem(key)
+
+}
+
+
+function  clearLocalStorage(){
+  localStorage.clear()
+
+}
+
+
