d42a593ec01710b155b6524de51a877ee52d7812
manik712 <ranineela71@gmail.com>
2024-04-21 23:16:17 +0600

    add async await

--- JSWebAPIs/FetchAPI.js
+++ JSWebAPIs/FetchAPI.js
@@ -1,10 +1,16 @@
 // Fetch API
 const display = document.getElementById("displays");
 
-function fetchData() {
-  fetch("http://127.0.0.1:5501/JSWebAPIs/fetchData.text")
-    .then((res) => res.text())
-    .then((data) => {
-      display.innerHTML = data;
-    });
+// function fetchData() {
+//   fetch("http://127.0.0.1:5501/JSWebAPIs/fetchData.text")
+//     .then((res) => res.text())
+//     .then((data) => {
+//       display.innerHTML = data;
+//     });
+// }
+
+async function fetchData() {
+  const res = await fetch("http://127.0.0.1:5501/JSWebAPIs/fetchData.text");
+  const data = await res.text();
+  display.innerHTML = data;
 }
