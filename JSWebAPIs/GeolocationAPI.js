2a91ef78ece272be60b2a1dff2602c54fcbca975
manik712 <ranineela71@gmail.com>
2024-04-22 10:56:40 +0600

    showError

--- JSWebAPIs/GeolocationAPI.js
+++ JSWebAPIs/GeolocationAPI.js
@@ -4,9 +4,11 @@
 
 const display = document.getElementById("display");
 
+// displayLocate
+
 function displayLocate() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
   } else {
     display.innerHTML = "Geolocation is not supported by this browser.";
   }
@@ -19,5 +21,26 @@ function showPosition(position) {
     "latitude:" +
     position.coords.latitude +
     "<br>Longitude:" +
-    position.coords.longitude;
+    position.coords.longitude +
+    "<br>altitude:" +
+    position.coords.heading;
+}
+
+// showError   
+
+function showError(error) {
+  switch (error.code) {
+    case error.PERMISSION_DENIED:
+      display.innerHTML = "User denied the request for Geolocation.";
+      break;
+    case error.POSITION_UNAVAILABLE:
+      display.innerHTML = "Location information is unavailable.";
+      break;
+    case error.TIMEOUT:
+      display.innerHTML = "The request to get user location timed out.";
+      break;
+    case error.UNKNOWN_ERROR:
+      display.innerHTML = "An unknown error occurred.";
+      break;
+  }
 }
