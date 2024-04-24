8a8291eb3a683d6cdd06274ca927582b5108cb29
manik712 <ranineela71@gmail.com>
2024-04-24 23:54:47 +0600

    add .onload = function

--- JSAJAX/lession-5/script.js
+++ JSAJAX/lession-5/script.js
@@ -1,13 +1,19 @@
-function getData(){
-  console.log("getData");
+function getData() {
+  const xhr =  new XMLHttpRequest();
 
+  xhr.onload = function () {
+    console.log(this.responseText);
+  };
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/todos/1");
+  xhr.send();                               
 }
 
-
-
-
-
-function sentData(){
-
-console.log("sentData");
+function sentData() {
+  console.log("sentData");
 }
+
+const getButton = document.getElementById("get");
+const sentButton = document.getElementById("sent");
+
+getButton.addEventListener("click", getData);
+sentButton.addEventListener("click", sentData);
