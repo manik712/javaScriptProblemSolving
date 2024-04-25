1712b2ce3453501f1da0d39a9f3ad7e47e838434
manik712 <ranineela71@gmail.com>
2024-04-25 09:20:52 +0600

    add url

--- JSAJAX/lession-5/script.js
+++ JSAJAX/lession-5/script.js
@@ -1,30 +1,28 @@
 // "https://jsonplaceholder.typicode.com/todos/1"
 
 
+
+
 //sendRequest
 
 function sendRequest(method, url) {
+  const promise = new Promise(function (resolve, reject) {
+    const xhr = new XMLHttpRequest();
 
-const promise =new Promise(function(resolve, reject) {
+    xhr.onload = function () {
+      // console.log(JSON.parse(this.responseText).userId);
+      //1
+      resolve(this.response);
+    };
+    xhr.open(method, url);
 
-  const xhr = new XMLHttpRequest();
+    xhr.send();
+    xhr.responseType = "json";
+  });
 
-  xhr.onload = function () {
-    // console.log(JSON.parse(this.responseText).userId);
-    //1
-   resolve(this.response)
-  };
-  xhr.open(method, url);
-
-  xhr.send();
-  xhr.responseType = "json";
-
-
-})
-
-return promise
-
+  return promise;
 }
+
 
 
 
@@ -32,16 +30,28 @@ return promise
 
 //getData....
 
-function getData() {}
+function getData() {
+  sendRequest("GET", "https://jsonplaceholder.typicode.com/todos/1").then(
+    (responseData) => {
+      console.log(responseData);
+    }
+  );
+}
+
 
 
 // sendData....
 function sentData() {
   console.log("sentData");
 }
+
+
+
 //getElement
 const getButton = document.getElementById("get");
 const sentButton = document.getElementById("sent");
+
+
 
 
 //event handlers
