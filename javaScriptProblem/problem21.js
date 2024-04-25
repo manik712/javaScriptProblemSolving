a8a1e15095734282960810493b68459e668d1e48
manik712 <ranineela71@gmail.com>
2024-04-25 23:24:48 +0600

    Create a function that takes length and width and finds the perimeter of a rectangle.

---
+++ javaScriptProblem/problem21.js
@@ -0,0 +1,12 @@
+/*
+
+
+Create a function that takes length and
+ width and finds the perimeter of a rectangle.
+
+*/
+
+function findPerimeter(length,width){
+	return 2*(length+width)
+}
+console.log(findPerimeter(3,8));
