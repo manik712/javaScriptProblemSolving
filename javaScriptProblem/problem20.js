66d54f0c8391a432bd34fde83a4f361a653fc11a
manik712 <ranineela71@gmail.com>
2024-04-25 23:08:57 +0600

    There is a single operator in JavaScript, capable of providing the remainder of a division operation. Two numbers are passed as parameters. The first parameter divided by the second parameter will have a remainder, possibly zero. Return that value.

---
+++ javaScriptProblem/problem20.js
@@ -0,0 +1,18 @@
+/*
+There is a single operator in JavaScript, capable of
+providing the remainder of a division operation. Two 
+numbers are passed as parameters.
+The first parameter divided by the second parameter will
+have a remainder, possibly zero. Return that value.
+
+*/
+
+
+
+function remainder(x, y) {
+	const z = x%y;
+	return z
+}
+ console.log(remainder(3,7));
+ //3
+
