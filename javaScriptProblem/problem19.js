fba2600ad3bcfad50a385dc1d2b706c38aff8206
manik712 <ranineela71@gmail.com>
2024-04-25 22:26:19 +0600

    Create a function that finds the maximum range of a triangle's third edge, where the side lengths are all integers.

---
+++ javaScriptProblem/problem19.js
@@ -0,0 +1,17 @@
+/*
+
+Create a function that finds the
+maximum range of a triangle's 
+third edge, where the side lengths
+are all integers.
+
+
+*/
+
+function thirdEdge(s1, s2) {
+  const s3 = s1 + s2 - 1;
+  return s3
+}
+
+console.log(thirdEdge(2,5));
+//6
