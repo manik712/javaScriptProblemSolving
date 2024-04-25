88ff5b2b0f713229c2d38910530d054201120217
manik712 <ranineela71@gmail.com>
2024-04-25 09:09:16 +0600

    add promise function

--- JSAJAX/lession-5/script.js
+++ JSAJAX/lession-5/script.js
@@ -1,19 +1,49 @@
-function getData() {
-  const xhr =  new XMLHttpRequest();
+// "https://jsonplaceholder.typicode.com/todos/1"
+
+
+//sendRequest
+
+function sendRequest(method, url) {
+
+const promise =new Promise(function(resolve, reject) {
+
+  const xhr = new XMLHttpRequest();
 
   xhr.onload = function () {
-    console.log(this.responseText);
+    // console.log(JSON.parse(this.responseText).userId);
+    //1
+   resolve(this.response)
   };
-  xhr.open("GET", "https://jsonplaceholder.typicode.com/todos/1");
-  xhr.send();                               
+  xhr.open(method, url);
+
+  xhr.send();
+  xhr.responseType = "json";
+
+
+})
+
+return promise
+
 }
 
+
+
+
+
+//getData....
+
+function getData() {}
+
+
+// sendData....
 function sentData() {
   console.log("sentData");
 }
-
+//getElement
 const getButton = document.getElementById("get");
 const sentButton = document.getElementById("sent");
 
+
+//event handlers
 getButton.addEventListener("click", getData);
 sentButton.addEventListener("click", sentData);
