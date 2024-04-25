57346977d09ac216e1ba918b3f77c40165be5aec
manik712 <ranineela71@gmail.com>
2024-04-25 21:15:59 +0600

    You are counting points for a basketball game, given the amount of 2-pointers scored and 3-pointers scored, find the final points for the team and return that value.

---
+++ javaScriptProblem/problem18.js
@@ -0,0 +1,10 @@
+//You are counting points for a basketball game, given the amount of 
+//2-pointers scored and 3-pointers scored, find the final points for the
+//team and return that value.
+
+function findFinalPoints(a,b){
+return a*2+b*3
+
+}
+console.log(findFinalPoints(3,4));
+//18
