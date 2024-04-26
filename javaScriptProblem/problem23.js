8acf34db01ff0766b07550e257976c4033202fcc
manik712 <ranineela71@gmail.com>
2024-04-26 08:10:03 +0600

    Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.

---
+++ javaScriptProblem/problem23.js
@@ -0,0 +1,21 @@
+/*
+
+Create a function that takes a number as
+its only argument and returns true if it's
+less than or equal to zero, otherwise return false.
+
+*/
+
+
+function lessThanOrEqualToZero(num) {
+  if (num <= 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+console.log(lessThanOrEqualToZero(0));
+//true
+
