9376752f4271674c42e5610d09200832bdf30cfe
manik712 <ranineela71@gmail.com>
2024-04-28 23:06:31 +0600

    Create a function that takes the number of wins,  draws and losses and calculates the number of  points a football team has obtained so far.

---
+++ javaScriptProblem/problem31.js
@@ -0,0 +1,25 @@
+/*
+Create a function that takes the number of wins,
+ draws and losses and calculates the number of 
+ points a football team has obtained so far.
+
+
+
+wins get 3 points
+draws get 1 point
+losses get 0 points
+
+
+
+*/
+
+
+function footballerTeamP(wins,draws,losses){
+// Calculate points
+const points = (wins * 3) + (draws * 1) + (losses * 0);
+return points;
+
+
+}
+
+console.log(calculatePoints(3, 1, 2)); 
