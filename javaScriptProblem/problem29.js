88fc8eabb21e1dca6098882b0a3f7f57951718c6
manik712 <ranineela71@gmail.com>
2024-04-28 22:39:17 +0600

    JavaScript has a logical operator &&. The && operator takes two boolean values, and returns true if both values are true.

---
+++ javaScriptProblem/problem29.js
@@ -0,0 +1,28 @@
+/*
+JavaScript has a logical operator &&.  
+The && operator takes two boolean values,
+ and returns true if both values are true.
+
+*/
+
+
+
+
+
+
+
+
+
+function and(a, b) {
+  if (a === true && b === true) {
+    return true;
+  } else if (a === true && b === false) {
+    return false;
+  } else if (a === false && b === true) {
+    return false;
+  } else if (a === false && b === false) {
+    return false;
+  }
+}
+
+console.log(and(true, false));
