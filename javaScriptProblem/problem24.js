1fd7b3b6aa35048f8fd3104f2b83b240c5283d52
manik712 <ranineela71@gmail.com>
2024-04-28 22:10:27 +0600

    A student learning JavaScript was trying to make a function. His code should concatenate a passed string name with string "Edabit" and store it in a variable called result. He needs your help  to fix this code.

---
+++ javaScriptProblem/problem24.js
@@ -0,0 +1,18 @@
+/*
+
+A student learning JavaScript was trying to 
+make a function. His code should concatenate a 
+passed string name with string "Edabit" and 
+store it in a variable called result. He needs your help
+ to fix this code.
+
+
+*/
+
+
+function stringAdd(name){
+  const newString = "edabit".concat(name)
+  
+  return newString
+}
+console.log(stringAdd("me"))
