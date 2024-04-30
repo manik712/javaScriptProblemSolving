ee951ce1e2abb6c44a5db9040112f15ec7c64613
manik712 <ranineela71@gmail.com>
2024-04-30 22:46:17 +0600

    Recursion: Length of a String

---
+++ javaScriptProblem/problem46.js
@@ -0,0 +1,17 @@
+/*
+
+Write a function that returns the length
+ of a string. Make your function recursive.
+
+*/
+
+function lengthOfString(string){
+    const result = string.length;
+    if(result!=0){
+      return "the length is" + " " + result;
+    }else{
+      return "the length is" + " " + result;
+    }
+}
+
+console.log(lengthOfString(' '));
