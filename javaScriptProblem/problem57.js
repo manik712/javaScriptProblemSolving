26c95799976cc42cf9cb2a00d036dde202ba1de9
manik712 <ranineela71@gmail.com>
2024-05-02 13:23:50 +0600

    Reverse an Array

---
+++ javaScriptProblem/problem57.js
@@ -0,0 +1,13 @@
+/*
+Write a function to reverse an array.
+
+*/
+
+function reverse(arr){
+     const myArray = arr;
+     console.log(myArray);
+     const reverseArray =myArray.reverse();
+     return reverseArray;
+}
+
+console.log(reverse([1,2,3,4,5]));
