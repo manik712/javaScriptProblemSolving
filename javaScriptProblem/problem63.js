1aff522c71e88040adfd8d12a64be0185659fbff
manik712 <ranineela71@gmail.com>
2024-05-05 22:15:48 +0600

    Using Ternary Operators

---
+++ javaScriptProblem/problem63.js
@@ -0,0 +1,18 @@
+/*
+
+
+
+Write a function that uses the ternary 
+operator to return "yeah" if bool is true, 
+and "nope" otherwise.
+
+
+
+*/
+
+
+function yeah_nope(bool) {
+	 return bool ? "yeah"  : " nope";
+}
+
+console.log(yeah_nope(true));
