edfb04111edb9e005d2fe1b7c7edd2b96a73ecca
manik712 <ranineela71@gmail.com>
2024-05-06 08:05:47 +0600

    ES6: Destructuring Arrays I

---
+++ javaScriptProblem/problem65.js
@@ -0,0 +1,17 @@
+/*
+
+
+
+With ES6, you can assign variables from arrays 
+in a much more succinct way. 
+Create variables a and b from the given array 
+using the ES6 destructuring assignment syntax, 
+where a === 1 and b === 2.
+
+
+*/
+
+const arr = [1, 2, 3, 4, 5, 6];
+ a = arr[0];
+ b = arr[3];
+ console.log(a, b);
