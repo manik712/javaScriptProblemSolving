b48a38e691cbf83099ea33691053463d6911cccf
manik712 <ranineela71@gmail.com>
2024-05-08 09:21:14 +0600

    Stack the Boxes

---
+++ javaScriptProblem/problem80.js
@@ -0,0 +1,20 @@
+/*
+Here's an image of four models. Some of the cubes 
+are hidden behind other cubes. Model one
+consists of one cube. Model two consists of
+four cubes, and so on...
+
+
+
+
+Write a function that takes a number n
+and returns the number of stacked boxes
+in a model n levels high, visible and 
+invisible.
+
+*/
+
+
+
+const stackBoxes = (n) => Math.pow(n, 2);
+console.log(stackBoxes(1));
