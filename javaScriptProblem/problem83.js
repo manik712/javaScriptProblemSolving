58ac2cf6b325ea1807a4e09b1199acf24ef7a4ef
manik712 <ranineela71@gmail.com>
2024-05-08 13:29:20 +0600

    Concatenating Two Integer Arrays

---
+++ javaScriptProblem/problem83.js
@@ -0,0 +1,11 @@
+/*
+
+Create a function to concatenate two integer arrays.
+
+*/
+
+function arrConcat(arr1, arr2){
+  const result = arr1.concat(arr2);
+  return result;
+}
+console.log(arrConcat([1,2,3],[4,5,6]));
