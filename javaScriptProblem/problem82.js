1380d3ec611024a055569f090dd65647063c1e4a
manik712 <ranineela71@gmail.com>
2024-05-08 12:01:53 +0600

    Convert an Array to a String

---
+++ javaScriptProblem/problem82.js
@@ -0,0 +1,14 @@
+/*
+
+Create a function that takes an array of numbers or letters and returns a string.
+
+*/
+
+function arrayToString(arr) {
+  const myArr =arr.toString();
+  return myArr;
+}
+
+
+console.log(arrayToString(['a', 'b', 'c']));
+//a,b,c
