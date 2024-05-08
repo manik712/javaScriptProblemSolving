fe7445034676f788ad344946a60a81ce35d2a5d9
manik712 <ranineela71@gmail.com>
2024-05-08 08:44:30 +0600

    Name Greeting! with arrow function

---
+++ javaScriptProblem/problem77.js
@@ -0,0 +1,13 @@
+/*
+
+Create a function that takes a name 
+and returns a greeting in the form of a
+string. Don't use a normal function, use
+an arrow function.
+
+*/
+
+const helloName = (name) => "Hello " +name+"!";
+
+
+console.log(helloName("manik"));
