0b22cc5f52228371b50e0308a12eb66f97b51b03
manik712 <ranineela71@gmail.com>
2024-05-09 11:10:52 +0600

    add

---
+++ javaScriptProblem/problem94.js
@@ -0,0 +1,19 @@
+/*
+
+A typical car can hold four passengers and one driver, allowing five
+people to travel around. Given n number of people,
+return how many cars are needed to seat everyone comfortably.
+
+*/
+
+function carNumber(n) {
+  const myCar = n / 5;
+  if (Number.isInteger(myCar)) {
+    return myCar;
+  } else if (!Number.isInteger(myCar)) {
+    const ourCar = Math.ceil(myCar);
+    return ourCar;
+  }
+}
+
+console.log(carNumber(13));
