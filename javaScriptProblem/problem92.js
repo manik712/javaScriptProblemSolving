2cdfd059edefaa50af8d8bf0f6e8e5e95dce4e0f
manik712 <ranineela71@gmail.com>
2024-05-09 09:55:47 +0600

    Number of Stickers

---
+++ javaScriptProblem/problem92.js
@@ -0,0 +1,16 @@
+/*
+
+
+Given a Rubik's Cube with a side length of n,
+return the number of individual stickers that
+are needed to cover the whole cube.
+
+
+*/
+
+function howManyStocks(n) {
+  const totalStocks = 6 * Math.pow(n, 2);
+  return totalStocks;
+}
+
+console.log(howManyStocks(3));
