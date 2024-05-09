48a2ebdc84c1dfbfafa205373873703a9d961418
manik712 <ranineela71@gmail.com>
2024-05-09 08:43:22 +0600

    Check if an Array Contains a Given Number

--- javaScriptProblem/problem90.js
+++ javaScriptProblem/problem90.js
@@ -1,6 +1,16 @@
 /*
 
-
+Write a function to check if an array contains a particular number.
 
 
 */
+
+function checkArrayContains(arr, num) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === num) {
+      return true;
+    }
+  }
+  return false;
+}
+console.log(checkArrayContains([],21));
