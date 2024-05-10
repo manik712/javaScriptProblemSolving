47f9313faee795ea850c4fddac2e016a299c1610
manik712 <ranineela71@gmail.com>
2024-05-10 23:03:20 +0600

    minimum number in an array

---
+++ javaScriptProblem/problem102.js
@@ -0,0 +1,18 @@
+/*
+
+Write a JavaScript program to find the minimum number in an array
+
+*/
+
+
+function findMaxNumber(arr) {
+  const myArr = arr;
+  let maxNumber = arr[0];
+  for (let i = 0; i < myArr.length; i++) {
+    if (myArr[i] < maxNumber) {
+      maxNumber = myArr[i];
+    }
+  }
+  return maxNumber;
+}
+console.log(findMaxNumber([3, 5, 8, 1, 90, 0, 56]));
