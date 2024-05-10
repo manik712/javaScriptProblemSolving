96c54a2a8451cad98391f6705d927334e311d31b
manik712 <ranineela71@gmail.com>
2024-05-10 23:01:32 +0600

    maximum number in an array.

---
+++ javaScriptProblem/problem101.js
@@ -0,0 +1,17 @@
+/*
+
+Write a JavaScript program to find the maximum number in an array. 
+
+*/
+
+function findMaxNumber(arr) {
+  const myArr = arr;
+  let maxNumber = arr[0];
+  for (let i = 0; i < myArr.length; i++) {
+    if (myArr[i] > maxNumber) {
+      maxNumber = myArr[i];
+    }
+  }
+  return maxNumber;
+}
+console.log(findMaxNumber([3, 5, 8, 1, 90, 0, 56]));
