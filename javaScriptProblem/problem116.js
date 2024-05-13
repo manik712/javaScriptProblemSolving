fcfe58c131e4b1a996bb636a1e8045e64f8c2ad4
manik712 <ranineela71@gmail.com>
2024-05-13 23:04:04 +0600

    Convert integers to strings

---
+++ javaScriptProblem/problem116.js
@@ -0,0 +1,19 @@
+/*
+
+Create a function that takes an array of integers and strings.
+Convert integers to strings and return the new array.
+
+*/
+function numberToString(arr) {
+  let arrStr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "number") {
+      const val = arr[i].toString();
+       arrStr.push(val);
+    }else if(typeof arr[i]==="string") {
+       arrStr.push(arr[i]);
+    }
+  }
+  return arrStr;
+}
+console.log(numberToString([2,3,4,5,"a","b","c","d","e","f",39,0,34]));
