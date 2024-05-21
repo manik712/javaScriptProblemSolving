98bb16084fe6bc96279af57c36d50c5d76d71cca
manik712 <ranineela71@gmail.com>
2024-05-21 14:09:22 +0600

    mood

---
+++ javaScriptProblem/problem121.js
@@ -0,0 +1,10 @@
+/*
+
+
+*/
+
+function moodToDay(mood){
+  const myMood = arguments.length;
+ return myMood ? `Today  i am felling ${mood}`:"Today i am felling netural"
+}
+console.log(moodToDay())
