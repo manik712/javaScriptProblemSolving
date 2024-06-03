ee8172768008936332f210fadfcef989147e71ae
manik712 <ranineela71@gmail.com>
2024-06-03 08:27:58 +0600

    Destructuring Arrays

---
+++ javaScriptProblem/problem127.js
@@ -0,0 +1,17 @@
+/*
+
+
+You can assign variables from arrays with destructuring like this:
+
+const arr = ["eyes", "nose", "lips", "ears"]
+let [eyes, nose, lips, ears] = arr
+But you can also skip over items in the array being destructured.
+
+
+
+*/
+
+let [eye, nose] = ["eyes", "nose"];
+
+
+console.log(eye);
