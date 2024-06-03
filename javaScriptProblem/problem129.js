a01fc08167386c607afb173ff4e4dd0be0e7f6d2
manik712 <ranineela71@gmail.com>
2024-06-03 21:14:12 +0600

    Half, Quarter and Eighth

---
+++ javaScriptProblem/problem129.js
@@ -0,0 +1,33 @@
+/*
+
+
+
+Create a function that takes a number and return an array of
+three numbers: half of the number, quarter of the number and
+an eighth of the number.
+
+
+
+Examples
+halfQuarterEighth(6) ➞ [3, 1.5, 0.75]
+
+halfQuarterEighth(22) ➞ [11, 5.5, 2.75]
+
+halfQuarterEighth(25) ➞ [12.5, 6.25, 3.125]
+
+
+*/
+
+function halfQuarterEighth(num) {
+  let arr = [];
+  if (num > 0) {
+    let halt = num / 2;
+    arr.push(halt);
+    let quarter = num/4;
+    arr.push(quarter);
+    let eighth = num/8;
+    arr.push(eighth);
+  }
+  return arr;
+}
+console.log(halfQuarterEighth(6));
