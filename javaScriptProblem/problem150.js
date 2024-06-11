5e79272680902f847bbbb9ba0a8876eef04a6c48
manik712 <ranineela71@gmail.com>
2024-06-11 23:17:35 +0600

    Add, Subtract, Multiply or Divide?

---
+++ javaScriptProblem/problem150.js
@@ -0,0 +1,31 @@
+/*
+
+Write a function that takes two numbers and returns
+if they should be added, subtracted, multiplied or divided to
+get 24. If none of the operations can give 24, return null.
+
+
+Examples
+
+operation(15, 9) ➞ "added"
+
+operation(26, 2) ➞ "subtracted"
+
+operation(11, 11) ➞ null
+
+*/
+
+function operation(num1, num2) {
+  if(num1 +num2 ===24){
+    return "add"
+  }else if (num1 * num2 ===24){
+    return "multiplied"
+  }else if (num1/num2===24){
+    return "divided"
+  }else{
+    return null
+  }
+
+}
+console.log(operation(2 ,48));
+
