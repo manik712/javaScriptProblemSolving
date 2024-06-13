b810d86588ccd5763d8a73a1d999e006c78e4e08
manik712 <ranineela71@gmail.com>
2024-06-13 11:01:52 +0600

    add alternative

--- javaScriptProblem/problem164.js
+++ javaScriptProblem/problem164.js
@@ -23,3 +23,40 @@ function getCase(word){
   }
 }
 console.log(getCase("ARROW"));
+
+
+
+// or
+
+
+function getCase(word) {
+  let hasUpper = false;
+  let hasLower = false;
+
+  for (let char of word) {
+    if (char >= 'A' && char <= 'Z') {
+      hasUpper = true;
+    } else if (char >= 'a' && char <= 'z') {
+      hasLower = true;
+    }
+  }
+
+  if (hasUpper && hasLower) {
+    return "mixed";
+  } else if (hasUpper) {
+    return "upper";
+  } else if (hasLower) {
+    return "lower";
+  } else {
+    return "mixed"; // If no alphabetic characters, consider as "mixed"
+  }
+}
+
+// Test cases
+
+console.log(getCase("whisper..."));  // ➞ "lower"
+console.log(getCase("SHOUT!"));      // ➞ "upper"
+console.log(getCase("Indoor Voice")); // ➞ "mixed"
+console.log(getCase("1234"));        // ➞ "mixed" (no alphabetic characters)
+console.log(getCase("1234A"));       // ➞ "upper" (only upper case letter)
+console.log(getCase("1234a"));       // ➞ "lower" (only lower case letter)
