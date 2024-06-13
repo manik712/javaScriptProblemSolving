fc90a8cef2fd0ce6705227cb76d41803e5877094
manik712 <ranineela71@gmail.com>
2024-06-13 09:15:46 +0600

    How Many D's Are There?

---
+++ javaScriptProblem/problem160.js
@@ -0,0 +1,22 @@
+/*
+Create a function that counts how many D's are in a sentence.
+Examples
+
+countDs("My friend Dylan got distracted in school.") ➞ 4
+
+countDs("Debris was scattered all over the yard.") ➞ 3
+
+countDs("The rodents hibernated in their den.") ➞ 3
+
+
+*/
+
+function countD(str) {
+  let myCount = str.toLowerCase();
+  let reg = /[d]/g;
+  let count1 = myCount.match(reg);
+  let count2 = count1.length;
+  return count2;
+}
+
+console.log(countD("The rodents hibernated in their den."));
