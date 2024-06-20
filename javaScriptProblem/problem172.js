18fd35e0b90d7162b936d3b6d61a52ca70847b8d
manik712 <ranineela71@gmail.com>
2024-06-20 14:30:39 +0600

    add

--- javaScriptProblem/problem172.js
+++ javaScriptProblem/problem172.js
@@ -2,15 +2,28 @@
 
 
 */
-function addIndex(arr){
-  let  myArr = []
-for(i=0; i<arr.length;i++){
+// function addIndex(arr){
+//   let  myArr = []
+// for(i=0; i<arr.length;i++){
   
-  let myIndex = indexOf(arr[i]);
-  let mySum = myIndex+arr[i];
-  myArr.push(mySum)
-}
-return myArr;
+//   let myIndex = indexOf(arr[i]);
+//   let mySum = myIndex+arr[i];
+//   myArr.push(mySum)
+// }
+// return myArr;
+// }
+
+// console.log(addIndex([0,1,2]))
+
+
+
+function addIndex(arr) {
+  let myArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    let mySum = i + arr[i];
+    myArr.push(mySum);
+  }
+  return myArr;
 }
 
-console.log(addIndex([0,1,2]))
+console.log(addIndex([0, 1, 2]));  // Output: [0, 2, 4]
