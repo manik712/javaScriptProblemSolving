f6481a47d92b20b968225fee40aeb72df0d263c9
manik712 <ranineela71@gmail.com>
2024-11-12 07:20:31 +0600

    add  this problem

---
+++ javaScriptProblem/problem182.js
@@ -0,0 +1,26 @@
+/*
+
+
+
+
+Try to make the solution to this problem as efficiently as possible.
+Consider using loops that would allow you to type the fewest characters/commands.
+While you could simply print the even numbers, get creative and 
+see how you could output them in a way that would work up to 10 or even up to 
+10,000 with little extra effort.
+
+
+
+*/
+
+function evenNumber(n) {
+  let arr = [];
+
+  for (let i = 2; i <= 10000; i += 2) {
+    arr.push(i);
+  }
+  return arr;
+
+}
+
+console.log(evenNumber(2));
