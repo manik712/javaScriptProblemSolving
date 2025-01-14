ac884f8e995828e4316e52c400643661a2f78b16
manik712 <ranineela71@gmail.com>
2025-01-14 08:42:46 +0600

    add sort an array

---
+++ javaScriptProblem/problem186.js
@@ -0,0 +1,10 @@
+/*
+Sort an array from lowest to highest
+*/
+
+const numbers = [45, 2, 78, 12, 99, 6];
+
+// Sort the array in ascending order
+numbers.sort((a, b) => a - b);
+
+console.log(numbers); // Output: [2, 6, 12, 45, 78, 99]
