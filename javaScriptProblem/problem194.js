f858d18ada0219987047ccaa8ad5cd97c5baad19
manik712 <ranineela71@gmail.com>
2026-09-07 21:44:35 +0600

    takes an array containing only numbers and return the first element

--- javaScriptProblem/problem194.js
+++ javaScriptProblem/problem194.js
@@ -1,1 +1,9 @@
 //Create a function that takes an array containing only numbers and return the first element.
+
+
+function getFirstElement(arr) {
+  return arr[0];
+}
+
+console.log(getFirstElement([10, 20, 30])); // 10
+console.log(getFirstElement([5, 8, 12]));  // 5
