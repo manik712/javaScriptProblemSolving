75d9b12ca8ffb933c4ab3b66a3e61015f0c2fce1
manik712 <ranineela71@gmail.com>
2026-09-07 08:55:53 +0600

    find the next number by adding 1

---
+++ javaScriptProblem/problem191.js
@@ -0,0 +1,11 @@
+//Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+function nextNumber(a){
+
+  const b=a+1;
+  return b;
+
+}
+
+
+console.log(nextNumber(-4))
