c7b645ca912efa7231b55e5d7cc7ce79a8129cf8
manik712 <ranineela71@gmail.com>
2026-09-09 21:31:47 +0600

    created an infinite loop

--- javaScriptProblem/problem196.js
+++ javaScriptProblem/problem196.js
@@ -1,14 +1,12 @@
 //Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
 
-function lessThanOrEqualToZero(number){
-  const a= number
-  if(a>0){
-    return true
-  }
-  else {
-    return false
+function lessThanOrEqualToZero(number) {
+  const a = number;
+  if (a > 0) {
+    return true;
+  } else {
+    return false;
   }
 }
 
-
-console.log(lessThanOrEqualToZero(0))
+console.log(lessThanOrEqualToZero(0));
