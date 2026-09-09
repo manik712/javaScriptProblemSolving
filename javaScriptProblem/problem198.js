af598f85abaf0666e3ca5a5e9a22ad034b4b6656
manik712 <ranineela71@gmail.com>
2026-09-09 21:36:08 +0600

    add the function

--- javaScriptProblem/problem198.js
+++ javaScriptProblem/problem198.js
@@ -1,1 +1,13 @@
 //In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals. The farmer breeds three species:
+
+
+function animals(chickens, cows, pigs) {
+	let x = chickens*2;
+let y = cows*4;
+let z =pigs*4;
+let a= x+y+z;
+return a;
+
+}
+
+console.log(animals(2,4,2))
