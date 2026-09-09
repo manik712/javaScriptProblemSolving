69783117287530c61ef142137ccdc7c6fa97ba33
manik712 <ranineela71@gmail.com>
2026-09-09 22:43:38 +0600

    function returns true if and only if x is equal to 7.

---
+++ javaScriptProblem/problem203.js
@@ -0,0 +1,10 @@
+//Fix the code in the Code tab so the function returns true if and only if x is equal to 7.
+function equalTo7(x) {
+  if (x === 7) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+console.log(equalTo7("7"))
