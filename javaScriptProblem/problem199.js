b5d26ae8df1cc2e97e1aff989127f3bc0dcccf36
manik712 <ranineela71@gmail.com>
2026-09-09 21:44:22 +0600

    return true if the sum of both numbers is less than 100. Otherwise return false.

--- javaScriptProblem/problem199.js
+++ javaScriptProblem/problem199.js
@@ -1,2 +1,12 @@
 //Given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false.
 
+function lessThan100(num1,num2){
+  const number = num1+num2;
+  if(number<100){
+    return true
+  }
+  else{
+    return false
+  }
+}
+console.log(lessThan100(12,20))
