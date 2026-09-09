da2f98b554f6df87c984b8aea0e99fec35ed5901
manik712 <ranineela71@gmail.com>
2026-09-09 22:02:15 +0600

    && operator takes two boolean values, and returns true if both values are true

--- javaScriptProblem/problem200.js
+++ javaScriptProblem/problem200.js
@@ -1,1 +1,10 @@
 //JavaScript has a logical operator &&. The && operator takes two boolean values, and returns true if both values are true.
+
+function and(a, b) {
+  if (a < 10 && b > 20) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(and(2, 21));
