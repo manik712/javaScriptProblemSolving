04919053123be4aeb275cfc501f0e482ca0404ac
manik712 <ranineela71@gmail.com>
2026-09-11 19:59:16 +0600

    is equal both number?

---
+++ javaScriptProblem/problem212.js
@@ -0,0 +1,9 @@
+//Create a function that returns true when num1 is equal to num2; otherwise return false.
+function isEqual(num1, num2) {
+  if (num1 === num2) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isEqual(5,"5"))
