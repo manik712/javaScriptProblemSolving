539813f92fae8e8986ed7cf78cb40274ce4ab1b7
manik712 <ranineela71@gmail.com>
2026-09-11 21:33:00 +0600

    takes a string and returns it as an integer

---
+++ javaScriptProblem/problem215.js
@@ -0,0 +1,10 @@
+//Create a function that takes a string and returns it as an integer.
+
+function stringToInteger(str) {
+  const a = Number(str);
+
+  // const b = typeof a;
+  return a;
+}
+
+console.log(stringToInteger("123"));
