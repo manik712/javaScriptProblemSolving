363b7af239f520a5591390dd9558d692c7e9dd7b
manik712 <ranineela71@gmail.com>
2026-09-13 18:48:46 +0600

    function accepts two parameters, age and isSupervised. Return a boolean.

---
+++ javaScriptProblem/problem221.js
@@ -0,0 +1,18 @@
+//Write a function that checks whether a person can watch an MA15+ rated movie. One of the following two conditions is required for admittance:
+
+//The person is at least 15 years old.
+
+//They have parental supervision.
+
+//The function accepts two parameters, age and isSupervised. Return a boolean.
+
+
+function acceptIntoMovie(age,condition){
+  if(age>=15 && condition===true){
+    return true
+  }
+  else{
+    return false
+  }
+}
+console.log(acceptIntoMovie(5,true))//false
