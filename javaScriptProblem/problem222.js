bf9e2b5ae326f5a2e9e6a47715edbc1a2f0afe62
manik712 <ranineela71@gmail.com>
2026-09-13 18:53:38 +0600

    radian to degree

---
+++ javaScriptProblem/problem222.js
@@ -0,0 +1,14 @@
+//Create a function that takes an angle in radians and returns the corresponding angle in degrees.
+
+//Examples
+
+//radiansToDegrees(1) ➞ 57.29577951308232 radiansToDegrees(20) ➞
+
+
+
+function radiansToDegrees(rad){
+  const x=(180/3.1416)*rad;
+  return x;
+}
+console.log(radiansToDegrees(1))//57.29564553
+
