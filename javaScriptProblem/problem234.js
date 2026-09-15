809069318e8e1eb3b81b63534d954f2c4f3e7e95
manik712 <ranineela71@gmail.com>
2026-09-15 23:30:06 +0600

    add the for loop,if condition

---
+++ javaScriptProblem/problem234.js
@@ -0,0 +1,9 @@
+function classifyNumbers(arr) {
+  for (i = 0; i <= arr.length - 1; i++) {
+    if (arr[i] % 2 === 0 && arr[i] % 5 === 0 && 50 > arr[i]) {
+      console.log(`${arr[i]}➤Even,Divisible by 5,below 50`);
+    }
+  }
+}
+classifyNumbers([10, 70]);
+
