de1120e9b6ed401a740ad7f9be2ff6ca33bba238
manik712 <ranineela71@gmail.com>
2026-09-15 23:38:51 +0600

    add the else if condition

--- javaScriptProblem/problem234.js
+++ javaScriptProblem/problem234.js
@@ -2,8 +2,10 @@ function classifyNumbers(arr) {
   for (i = 0; i <= arr.length - 1; i++) {
     if (arr[i] % 2 === 0 && arr[i] % 5 === 0 && 50 > arr[i]) {
       console.log(`${arr[i]}➤Even,Divisible by 5,below 50`);
+    }else if(arr[i] % 2 !== 0 && arr[i] % 5 !== 0 && 50 < arr[i]){
+       console.log(`${arr[i]}➤Odd,Divisible by 5,upto 50`);
     }
   }
 }
-classifyNumbers([10, 70]);
+classifyNumbers([10, 70,57]);
 
