f70e6f442b9a31c55e2f5f6f02a3f0c1443769e3
manik712 <ranineela71@gmail.com>
2026-09-20 23:21:54 +0600

    Two Lists inside a List to One array

---
+++ javaScriptProblem/problem248.js
@@ -0,0 +1,17 @@
+//The input contains exactly two subArrays.
+//Return one array containing the items from the
+//first subarray followed by the items from the second.
+
+//Examples
+//oneList([[1, 2], [3, 4]]) // [1, 2, 3, 4]
+//oneList([["a", "b"], ["c", "d"]]) // ["a", "b", "c", "d"]
+//oneList([[true, false], [false, false]]) // [true, false, false, false]
+
+//method1
+function oneList(arr1, arr2) {
+  let x = [...arr1, ...arr2];
+  return x;
+}
+
+console.log(oneList([1, 2, 3], [8, 7, 6]));//[ 1, 2, 3, 8, 7, 6 ]
+
