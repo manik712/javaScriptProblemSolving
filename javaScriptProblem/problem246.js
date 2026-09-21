59c19b166fe855117d9e454b77ce9ab87e57b32d
manik712 <ranineela71@gmail.com>
2026-09-21 22:23:47 +0600

    split the string

--- javaScriptProblem/problem246.js
+++ javaScriptProblem/problem246.js
@@ -1,1 +1,14 @@
-//
+//Repeat the first two characters followed
+//by an ellipsis and space twice, then append
+//the full word and an exclamation mark.
+
+//Example
+//stuttering("astounding") ➞ "as... as... astounding!"
+
+function stuttering(str) {
+  //split the string
+  let x = str.split("");
+  
+}
+//call function
+stuttering("astounding");
