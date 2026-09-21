bd4478dbb10d9b807444c3c211144b1b3ea953c2
manik712 <ranineela71@gmail.com>
2026-09-21 08:36:45 +0600

    swap two digits of a number

---
+++ javaScriptProblem/problem250.js
@@ -0,0 +1,30 @@
+//Write a function that takes a two-digit
+//number and determines if it's the largest
+//of two possible digit swaps.
+
+// To illustrate:
+// largestSwap(27) ➞ false
+// largestSwap(43) ➞ true
+// If 27 is our input, we should return false
+//  because swapping the digits gives us 72,
+//  and 72 > 27. On the other hand, swapping
+//  43 gives us 34, and 43 > 34.
+
+// Examples
+// largestSwap(14) ➞ false
+// largestSwap(53) ➞ true
+// largestSwap(99) ➞ true
+
+function largestSwap(num) {
+  let swap = num.toString().split("");
+  let x = `${swap[1]}${swap[0]}`;
+  let number = Number(x);
+  if (num < number) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+console.log(largestSwap(99)); //true
+console.log(largestSwap(14)); //false
