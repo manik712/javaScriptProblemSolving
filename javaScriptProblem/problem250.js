b986a68c898130f1e764b7f40bb46b6f053c5279
manik712 <ranineela71@gmail.com>
2026-09-21 22:15:46 +0600

    added the comments

--- javaScriptProblem/problem250.js
+++ javaScriptProblem/problem250.js
@@ -9,22 +9,24 @@
 //  because swapping the digits gives us 72,
 //  and 72 > 27. On the other hand, swapping
 //  43 gives us 34, and 43 > 34.
-
+     
 // Examples
 // largestSwap(14) ➞ false
 // largestSwap(53) ➞ true
 // largestSwap(99) ➞ true
 
 function largestSwap(num) {
+  //make the number string and split them (make array)
   let swap = num.toString().split("");
   let x = `${swap[1]}${swap[0]}`;
   let number = Number(x);
+  //if else condition...
   if (num < number) {
     return false;
   } else {
     return true;
   }
 }
-
+//call and print the result
 console.log(largestSwap(99)); //true
 console.log(largestSwap(14)); //false
