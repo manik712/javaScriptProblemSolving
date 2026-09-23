196dac7e3867d0cc0a5544abcad738de0cde6ffe
manik712 <ranineela71@gmail.com>
2026-09-23 08:38:16 +0600

    add the function that count the letter by using match

--- javaScriptProblem/problem257.js
+++ javaScriptProblem/problem257.js
@@ -1,7 +1,6 @@
 //Create a function that takes two strings as arguments and
-//  returns the number of times the first string (the single character) 
+//  returns the number of times the first string (the single character)
 // is found in the second string.
-
 
 // Examples
 
@@ -12,12 +11,18 @@
 // Notes
 // Your output must be case-sensitive (see second example).
 
-function charCount(x,str){
+function charCount(x, str) {
+  let count = 0;
   //let match the letter ...
-   const regex = new RegExp(x, 'g'); 
+  const regex = new RegExp(x, "g");
   //  console.log(regex)
-  let y = str.match(regex)
-  console.log(y)
+  let y = str.match(regex);
+  //print the array
+  console.log(y);
+  for (let i = 0; i <= y.length - 1; i++) {
+    count++;
+  }
+  return count;
 }
 
-charCount("c", "Chamber of secrets")
+console.log(charCount("c", "Chamber of secrets"));
